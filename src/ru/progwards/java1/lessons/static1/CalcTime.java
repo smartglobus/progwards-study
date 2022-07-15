209487e67b5ccf4d4f7884a2671539b6ed3faeb8
smartglobus <smartglobus@gmail.com>
2022-07-15 23:58:38 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/static1/CalcTime.java
@@ -0,0 +1,8 @@
+package ru.progwards.java1.lessons.static1;
+
+public class CalcTime {
+
+    public static void main(String[] args){
+
+    }
+}
--- src/ru/progwards/smartglobus/Test.java
+++ src/ru/progwards/smartglobus/Test.java
@@ -1,11 +1,13 @@
 package ru.progwards.smartglobus;
 
 public class Test {
+    static String a = "3пролл  'о'ор'  оро";
+
     public static void main(String[] args) {
        long tt = 123;
         { String a = "2"; }
         String a = "3пролл  'о'ор'  оро";
         String n = "1\" 5845" + 5 + 8;
-        System.out.println(n);
+        System.out.println(Variables.sum);
     }
 }
