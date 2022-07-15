110777f87f7a720d6048c59de9c93eee06aa890a
smartglobus <smartglobus@gmail.com>
2022-07-16 00:05:03 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/static1/CalcTime.java
+++ src/ru/progwards/java1/lessons/static1/CalcTime.java
@@ -1,6 +1,14 @@
 package ru.progwards.java1.lessons.static1;
 
 public class CalcTime {
+    static double distance;
+    static double speed;
+    static String cityFrom;
+    static String cityTo;
+
+    public static void printTime(){
+        System.out.println("Если ехать из ? в ? со скоростью ? км/ч, то ? км проедем за ? часов.");
+    }
 
     public static void main(String[] args){
 
