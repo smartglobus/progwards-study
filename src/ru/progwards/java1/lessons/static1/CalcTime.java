14d648580cbbf60ab117dbb7d56cd924a8111f18
smartglobus <smartglobus@gmail.com>
2022-07-16 15:52:58 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/static1/CalcTime.java
+++ src/ru/progwards/java1/lessons/static1/CalcTime.java
@@ -11,7 +11,37 @@ public class CalcTime {
         System.out.println("Если ехать из "+cityFrom+" в "+cityTo+" со скоростью "+speed+" км/ч, то "+distance+" км проедем за "+t+" часов.");
     }
 
-    public static void main(String[] args){
+    public static void MoscowToPiter(){
+        distance = 712;
+        speed = 80;
+        cityFrom = "Москва";
+        cityTo = "Питер";
+    }
 
+    public static void LissabonToVladivostok(){
+        distance = 13090;
+        speed = 75;
+        cityFrom = "Лиссабон";
+        cityTo = "Владивосток";
+    }
+
+    public static void MurmanskToAlmata(){
+        distance = 5413;
+        speed = 60;
+        cityFrom = "Мурманск";
+        cityTo = "Алмата";
+    }
+
+    public static void printTimes(){
+        MoscowToPiter();
+        printTime();
+        LissabonToVladivostok();
+        printTime();
+        MurmanskToAlmata();
+        printTime();
+    }
+
+    public static void main(String[] args){
+        printTimes();
     }
 }
---
+++ src/ru/progwards/java1/lessons/static1/Goodies.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.static1;
+
+public class Goodies {
+
+
+
+
+
+
+
+
+}
