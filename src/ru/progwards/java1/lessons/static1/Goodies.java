cb83500ea7af6a8263364b17bcede944eb4db81a
smartglobus <smartglobus@gmail.com>
2022-07-16 16:18:54 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/static1/Goodies.java
+++ src/ru/progwards/java1/lessons/static1/Goodies.java
@@ -1,12 +1,37 @@
 package ru.progwards.java1.lessons.static1;
 
 public class Goodies {
+    static String goodies;
+    static String child;
 
+    public static void printPresent(){
+        System.out.println(child + " дали " + goodies);
+    }
 
+    public static void masha(){
+        child = "Маше";
+    }
 
+    public static void cookie(){
+        goodies = "печеньку";
+    }
 
+    public static void setChild(String child){
+        child = child;
+    }
 
+    public static void setGoodies(String goodies){
+        goodies = goodies;
+    }
 
-
+    public static void printPresents(){
+        masha();
+        printPresent();
+        setGoodies("леденец");
+        printPresent();
+        cookie();
+        printPresent();
+        setChild("Пете");
+    }
 
 }
