ce64290bff009891e867d2db5852fdc1426ed6e7
smartglobus <smartglobus@gmail.com>
2022-07-18 23:07:25 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/wrappers/NumMetrics.java
@@ -0,0 +1,25 @@
+package ru.progwards.java1.lessons.wrappers;
+
+public class NumMetrics {
+
+    public static Integer sumDigits(Integer number){
+
+        int hundreds = number/100;
+        int dozens = number%100/10;
+        int digits = number%10;
+        return Integer.valueOf(hundreds+dozens+digits);
+    }
+
+    public static Integer mulDigits(Integer number){
+
+        int hundreds = number/100;
+        int dozens = number%100/10;
+        int digits = number%10;
+        return Integer.valueOf(hundreds*dozens*digits);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(423));
+        System.out.println(mulDigits(423));
+    }
+}
