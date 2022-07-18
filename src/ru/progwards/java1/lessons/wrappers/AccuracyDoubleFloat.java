6bc8a32511e25b663dcc9490a1f0c8a86da64e94
smartglobus <smartglobus@gmail.com>
2022-07-19 00:55:08 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/wrappers/AccuracyDoubleFloat.java
@@ -0,0 +1,26 @@
+package ru.progwards.java1.lessons.wrappers;
+
+public class AccuracyDoubleFloat {
+    static final double R_D = 6371.2;
+    static final float R_F = 6371.2f;
+    static final double PI_D = 3.14;
+    static final float PI_F = 3.14f;
+
+    public static Double volumeBallDouble(Double radius){
+        return R_D*R_D*R_D*PI_D*4/3;
+    }
+
+    public static Float volumeBallFloat(Float radius){
+        return R_F*R_F*R_F*PI_F*4/3;
+    }
+
+    public static Double calculateAccuracy(Double radius){
+        return volumeBallDouble(radius) - volumeBallFloat(radius.floatValue());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(volumeBallDouble(R_D));
+        System.out.println(volumeBallFloat(R_F));
+        System.out.println(calculateAccuracy(R_D));
+    }
+}
