c9d36c755ef15db809c457bc5b73cde9b1b31406
smartglobus <smartglobus@gmail.com>
2022-07-19 22:27:31 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/custing/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/custing/AccuracyDoubleFloat.java
@@ -1,7 +1,14 @@
 package ru.progwards.java1.lessons.custing;
 
 public class AccuracyDoubleFloat {
-    public static void main(String[] args) {
 
+    public static double calculateAccuracy() {
+        double d = 1. / 3;
+        float f = (float) d;
+        return d - f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateAccuracy());
     }
 }
