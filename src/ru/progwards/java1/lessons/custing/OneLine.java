84cea2f5a1afc6988b2b883b07e1e17e5447b5c7
smartglobus <smartglobus@gmail.com>
2022-07-19 22:10:00 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/custing/OneLine.java
+++ src/ru/progwards/java1/lessons/custing/OneLine.java
@@ -10,8 +10,18 @@ public class OneLine {
         return "Значение " + variable + " равно " + value;
     }
 
+    public static String square(int n) {
+        return "Число " + n + " в квадрате равно " + n * n;
+    }
+
+    public static String sum(int n, int m) {
+        return "Сумма " + n + " и " + m + " равна " + (n + m);
+    }
+
     public static void main(String[] args) {
         System.out.println(strValue("высокой важности"));
         System.out.println(intValue("высокой важности", 100500));
+        System.out.println(square(25));
+        System.out.println(sum(45, 78));
     }
 }
