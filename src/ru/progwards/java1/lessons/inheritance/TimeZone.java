941abc136063d23c5854f6ba95a8363fb5a90be2
smartglobus <smartglobus@gmail.com>
2022-08-30 23:26:53 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/inheritance/TimeZone.java
+++ src/ru/progwards/java1/lessons/inheritance/TimeZone.java
@@ -38,29 +38,39 @@ class ZonedTime extends Time {
 
     @Override
     public int secondsBetween(Time time) {
+        int seconds1;
         int seconds2;
-        int seconds1 = this.toSeconds() + this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60;
+
+        if (this.getTimeZone() == null) {
+            seconds1 = this.toSeconds();
+        }
+        seconds1 = this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60 + this.toSeconds();
+
         if (time.getTimeZone() == null) {
             seconds2 = time.toSeconds();
         }
-        seconds2 = time.toSeconds() + time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60;
+        seconds2 = time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60 + time.toSeconds();
 
         return Math.abs(seconds1 - seconds2);
     }
 
     public static void main(String[] args) {
-        TimeZone toronto = new TimeZone(-4);
-        TimeZone teheran = new TimeZone(4, 30);
-        TimeZone london = new TimeZone(0);
-        Time nullTimeZone = new Time(12, 40, 25);
-        ZonedTime gmtLondon = new ZonedTime(5, 15, 7, london);
-        ZonedTime gmt = new ZonedTime(15, 30, 0);
-        ZonedTime gmtPlus4_30 = new ZonedTime(15, 30, 45, teheran);
-        ZonedTime gmtMinus4 = new ZonedTime(7, 55, 30, toronto);
-        System.out.println(gmt.secondsBetween(gmtPlus4_30));
-        System.out.println(gmtMinus4.secondsBetween(gmtPlus4_30));
-        System.out.println(nullTimeZone.secondsBetween(gmt));
-        System.out.println(gmtLondon.secondsBetween(gmtPlus4_30));
+//        TimeZone toronto = new TimeZone(-4);
+//        TimeZone teheran = new TimeZone(4, 30);
+//        TimeZone london = new TimeZone(0);
+//        Time nullTimeZone = new Time(12, 40, 25);
+//        ZonedTime gmtLondon = new ZonedTime(5, 15, 7, london);
+//        ZonedTime gmt = new ZonedTime(15, 30, 0);
+//        ZonedTime gmtPlus4_30 = new ZonedTime(15, 30, 45, teheran);
+//        ZonedTime gmtMinus4 = new ZonedTime(7, 55, 30, toronto);
+        ZonedTime zt1 = new ZonedTime(6, 33, 8, new TimeZone(-1));
+        ZonedTime zt2 = new ZonedTime(6, 33, 8, new TimeZone(2));
+        Time teaTime = new Time(7, 8, 9);
+        System.out.println(zt1.secondsBetween(zt2));
+//        System.out.println(gmt.secondsBetween(gmtPlus4_30));
+//        System.out.println(gmtMinus4.secondsBetween(gmtPlus4_30));
+//        System.out.println(nullTimeZone.secondsBetween(gmt));
+//        System.out.println(gmtLondon.secondsBetween(gmtPlus4_30));
     }
 }
 
