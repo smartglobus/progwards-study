4620bf695a133b2ff3d6e223940d73040b66c924
smartglobus <smartglobus@gmail.com>
2022-09-03 11:26:08 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/inheritance/TimeZone.java
+++ src/ru/progwards/java1/lessons/inheritance/TimeZone.java
@@ -38,28 +38,39 @@ class ZonedTime extends Time {
 
     @Override
     public int secondsBetween(Time time) {
-        TimeZone k1 = this.getTimeZone();
-        TimeZone k2 = time.getTimeZone();
-        int sdvigk1, sdvigk2;
+        int seconds1ByGMT;
+        int seconds2ByGMT;
 
-        if (k1 == null)
-            sdvigk1 = 0;
-        else
-            sdvigk1 = k1.hours * 60 * 60 + k1.minutes * 60;
-        if (k2 == null)
-            sdvigk2 = 0;
-        else
-            sdvigk2 = k2.hours * 60 * 60 + k2.minutes * 60;
-        return Math.abs(super.secondsBetween(time) - sdvigk1 - sdvigk2);
+
+        if (this.getTimeZone() == null) {
+            seconds1ByGMT = this.toSeconds();
+        } else {
+            if (this.getTimeZone().hours < 0) {
+                seconds1ByGMT = this.toSeconds() - this.getTimeZone().hours * 3600 + this.getTimeZone().minutes * 60;
+            } else {
+                seconds1ByGMT = this.toSeconds() - this.getTimeZone().hours * 3600 - this.getTimeZone().minutes * 60;
+            }
+        }
+
+        if (time.getTimeZone() == null) {
+            seconds2ByGMT = time.toSeconds();
+        } else {
+            if (time.getTimeZone().hours < 0) {
+                seconds2ByGMT = time.toSeconds() - (time.getTimeZone().hours * 3600 + time.getTimeZone().minutes * 60);
+            } else {
+                seconds2ByGMT = time.toSeconds() - (time.getTimeZone().hours * 3600 - time.getTimeZone().minutes * 60);
+            }
+        }
+        return Math.abs(seconds1ByGMT - seconds2ByGMT);
     }
 
     public static void main(String[] args) {
 
         ZonedTime zt1 = new ZonedTime(6, 33, 8, new TimeZone(-1));
-        ZonedTime zt2 = new ZonedTime(4, 11, 2, new TimeZone(2));
+        ZonedTime zt2 = new ZonedTime(6, 33, 8, new TimeZone(2));
         System.out.println(zt1.secondsBetween(zt2));
-        ZonedTime zt3 = new ZonedTime(18, 7, 43, new TimeZone(-2,5));
-        Time t4 = new Time(16, 39, 35);
+        ZonedTime zt3 = new ZonedTime(18, 10, 43, new TimeZone(-2, 5));
+        Time t4 = new Time(18, 40, 44);
         System.out.println(zt3.secondsBetween(t4));
 
     }
