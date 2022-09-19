172b6b73d9564562de9affd2e730bf13d90a87af
smartglobus <smartglobus@gmail.com>
2022-09-19 19:02:01 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/arrays1/Matrix.java
+++ src/ru/progwards/java1/lessons/arrays1/Matrix.java
@@ -56,51 +56,50 @@ public class Matrix {
             return null;
         }
         int[][] transMatrix = new int[matrix[0].length][matrix.length];
-        System.out.println(transMatrix.length);
-        System.out.println(transMatrix[0].length);
-        System.out.println("");
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-               transMatrix[j][i] = matrix[i][j];
+                transMatrix[j][i] = matrix[i][j];
             }
         }
         return transMatrix;
     }
 
+
     public static void main(String[] args) {
         int[][] abc = {{-1, 2, 36, 4, 555}, {4, 48, 0}, {65, 128, -45, -0, 30, 3, 4, 5}};
         Matrix multiTest = new Matrix(abc);
-        //int[][] trueMatrix = new int[3][4];
+
         int[] a = {1, 2, 3, 4};
         int[] b = {5, 6, 7, 8};
         int[] c = {9, 10, 11, 12};
-        int[][]   trueMatrix = {a,b,c};
+        int[][] trueMatrix = {a, b, c};
         Matrix threeByFour = new Matrix(trueMatrix);
 
-//        System.out.println(multiTest.maxInRow(0));
-//        System.out.println(multiTest.maxInRow(1));
-//        System.out.println(multiTest.maxInRow(2));
-//        System.out.println(multiTest.maxInCol(5));
-//        System.out.println(multiTest.maxInCol(1));
-//        System.out.println(multiTest.max());
-//        System.out.println(multiTest.isMatrix());
+        System.out.println(multiTest.maxInRow(0));
+        System.out.println(multiTest.maxInRow(1));
+        System.out.println(multiTest.maxInRow(2));
+        System.out.println(multiTest.maxInCol(5));
+        System.out.println(multiTest.maxInCol(1));
+        System.out.println(multiTest.max());
+        System.out.println(multiTest.isMatrix());
         System.out.println(threeByFour.isMatrix());
 
         System.out.println(trueMatrix.length);
         System.out.println(trueMatrix[0].length);
         System.out.println("");
-        for (int[]row:threeByFour.getMatrix()
-             ) {
-            for (int value: row
-                 ) {
+
+        for (int[] row : threeByFour.getMatrix()
+        ) {
+            for (int value : row
+            ) {
                 System.out.println(value);
             }
         }
         System.out.println("");
-        threeByFour.transposition();
-        for (int[]row:threeByFour.getMatrix()
+        for (int[] row : threeByFour.transposition()
         ) {
-            for (int value: row
+            for (int value : row
             ) {
                 System.out.println(value);
             }
