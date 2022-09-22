421a0f4bd354922e5fd64a8ffbd93816f342858b
smartglobus <smartglobus@gmail.com>
2022-09-22 22:04:19 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/arrays2/IntArrays.java
+++ src/ru/progwards/java1/lessons/arrays2/IntArrays.java
@@ -37,10 +37,9 @@ public class IntArrays {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] > a[j]) {
-                    // Способ поменять местами значения в ячейках памяти без создания третьей переменной
-                    a[i] = a[j] - a[i];
-                    a[j] -= a[i];
-                    a[i] += a[j];
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
                 }
             }
         }
