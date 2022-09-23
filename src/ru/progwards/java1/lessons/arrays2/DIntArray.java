c0e9317db43e16032ac8a7db65509e903552504e
smartglobus <smartglobus@gmail.com>
2022-09-23 11:05:27 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -0,0 +1,60 @@
+package ru.progwards.java1.lessons.arrays2;
+
+import java.util.Arrays;
+
+public class DIntArray {
+    private int[] dIntArray;
+
+    DIntArray() {
+
+    }
+
+    public void add(int num) {
+
+        int[] newArray = Arrays.copyOf(dIntArray, dIntArray.length + 1);
+        newArray[newArray.length - 1] = num;
+        dIntArray = Arrays.copyOf(newArray, newArray.length);
+    }
+
+    public void atInsert(int pos, int num) {
+        int[] newArray;
+        newArray = Arrays.copyOf(dIntArray, dIntArray.length + 1);
+        for (int i = newArray.length - 1; i > pos; i--) {
+            newArray[i] = newArray[i - 1];
+        }
+        newArray[pos] = num;
+        dIntArray = Arrays.copyOf(newArray, newArray.length);
+    }
+
+    public void atDelete(int pos) {
+
+        for (int i = pos; i < dIntArray.length - 1; i++) {
+            dIntArray[i] = dIntArray[i + 1];
+        }
+        dIntArray = Arrays.copyOf(dIntArray, dIntArray.length - 1);
+    }
+
+    public int at(int pos) {
+        return dIntArray[pos];
+    }
+
+    public static void main(String[] args) {
+        DIntArray testArray = new DIntArray();
+        int[] a = {4, 6, 9, 17, 35};
+        testArray.setdIntArray(a);
+        testArray.add(5);
+        System.out.println(Arrays.toString(testArray.getdIntArray()));
+        testArray.atInsert(3, 100);
+        System.out.println(Arrays.toString(testArray.getdIntArray()));
+        testArray.atDelete(2);
+        System.out.println(Arrays.toString(testArray.getdIntArray()));
+    }
+
+    public void setdIntArray(int[] dIntArray) {
+        this.dIntArray = dIntArray;
+    }
+
+    public int[] getdIntArray() {
+        return dIntArray;
+    }
+}
