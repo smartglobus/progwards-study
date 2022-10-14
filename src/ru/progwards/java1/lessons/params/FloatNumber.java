f8adbe8c4db338582facc72711a74e8108f68b3c
smartglobus <smartglobus@gmail.com>
2022-10-14 21:54:03 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/params/FloatNumber.java
+++ src/ru/progwards/java1/lessons/params/FloatNumber.java
@@ -22,18 +22,20 @@ public class FloatNumber {
             sign = false;
         }
 
-
         // Нахождение ключевых точек
         long extrMnt = 0;
         int findDot = numToArray.length - 1; // первая значимая цифра
         int findE = numToArray.length - 1; // положение 'E'
         int lastNum = numToArray.length - 1; // последняя значимая цифра, или последняя цифра перед 'Е', если 'Е' обнаружится
+        boolean isE = false; // проверка наличия 'E'
+        int dotCount = 0;
 
         for (int i = 0; i < numToArray.length; i++) {
 
             if (numToArray[i] == 'e' || numToArray[i] == 'E') {
                 findE = i;
                 lastNum = i - 1;
+                isE = true;
             }
             if (numToArray[i] == '.') {
                 findDot = i;
@@ -51,36 +53,41 @@ public class FloatNumber {
 // если extrMnt >= Long.MAX_VALUE перестать заполнять мантиссу и перейти к окончательному вычислению степени
             if (extrMnt >= Long.MAX_VALUE / 10) {
 //коррекция exp по разнице между findDot и i на момент окончания заполнения мантиссы, c поправкой на возможный знак в начале
-                exp = exp + lastNum - i;
+                exp = exp + lastNum + dotCount - i;
                 break;
             }
             // не берёт в расчёт символы, не являющиеся цифрами ('-','+','.', и любые другие)
-            if (!Character.isDigit(numToArray[i])) {
-                exp++;
+            if (numToArray[i] == '.') {
+                dotCount++;
                 continue;
             }
-            extrMnt = extrMnt * 10 + Character.digit(numToArray[i], 10);
+            if (Character.isDigit(numToArray[i])) {
+                extrMnt = extrMnt * 10 + Character.digit(numToArray[i], 10);
+            }
         }
         mantissa = extrMnt;
 
         System.out.println("E = " + findE + ", lastNum = " + lastNum + ", Dot = " + findDot + ", exp = " + exp);
 
 // блок поправки к ехр на основании значения 'E'
-        boolean expSign;
-        if (numToArray[findE + 1] != '-') {
-            expSign = true;
-        } else {
-            expSign = false;
-        }
-
+        boolean expSign = true;
         int addToExp = 0;
-        for (int i = findE + 1; i < numToArray.length; i++) {
+//если 'E' найдена, определяем знак степени
+        if (isE) {
+
+            if (numToArray[findE + 1] != '-') {
+                expSign = true;
+            } else {
+                expSign = false;
+            }
+
+        for (int i = findE+1; i < numToArray.length; i++) {
             if (!Character.isDigit(numToArray[i])) {
                 continue;
             }
             addToExp = addToExp * 10 + Character.digit(numToArray[i], 10);
         }
-
+    }
         if (expSign) {
             exp += addToExp;
         } else {
@@ -88,20 +95,44 @@ public class FloatNumber {
         }
     }
 
-
     public static void main(String[] args) {
-        FloatNumber test = new FloatNumber("123456789876543.2123451E-250");
+        FloatNumber test = new FloatNumber(".103456789876543234567898765");
         System.out.println(test);
-
-
     }
 
     @Override
     public String toString() {
-        return "FloatNumber{" +
-                "sign=" + sign +
-                ", mantissa=" + mantissa +
-                ", exp=" + exp +
-                '}';
+        long mantissaFirstDigit = mantissa;
+        long mantissaWithoutFirstDigit = 0;
+        int expCorrToStd = 0;
+
+        while (mantissaFirstDigit >= 10) {
+            mantissaFirstDigit /= 10;
+            expCorrToStd++;
+        }
+        long dosensTimesExpCorr = 0;
+        if (mantissa >= 10) {
+            dosensTimesExpCorr = 1;
+            for (int i = 0; i < expCorrToStd; i++) {
+                dosensTimesExpCorr *= 10;
+            }
+        }
+
+        mantissaWithoutFirstDigit = mantissa - mantissaFirstDigit * dosensTimesExpCorr;
+        System.out.println("mantissaFirstDigit = " + mantissaFirstDigit + ", expCorrToStd = " + expCorrToStd + ", dosensTimesExpCorr = " + dosensTimesExpCorr);
+        System.out.println("mantissa = " + mantissa);
+        System.out.println("mantissaWithoutFirstDigit = " + mantissaWithoutFirstDigit + " corr = " + expCorrToStd);
+
+// первая цифра плюс знак
+        if (!sign) {
+            mantissaFirstDigit *= -1;
+        }
+
+// если 'E' отлична от нуля
+        if (exp + expCorrToStd != 0) {
+            return mantissaFirstDigit + "." + mantissaWithoutFirstDigit + "E" + (exp + expCorrToStd);
+        }
+
+        return mantissaFirstDigit + "." + mantissaWithoutFirstDigit;
     }
 }
