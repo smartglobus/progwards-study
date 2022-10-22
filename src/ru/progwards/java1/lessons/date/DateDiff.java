95cd0b8e35e0ac561589a7ba158548d968721a29
smartglobus <smartglobus@gmail.com>
2022-10-22 16:35:47 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/date/DateDiff.java
+++ src/ru/progwards/java1/lessons/date/DateDiff.java
@@ -11,14 +11,6 @@ public class DateDiff {
 
         zeroTime.clear();
         timeBtwCal.clear();
-//        d1Cal.clear();
-//        d2Cal.clear();
-//
-////        d1Cal.setTime(date1);
-//        d2Cal.setTime(date2);
-//
-//        d2Cal.set(1980,0,1,0,0,0);
-//        System.out.println(d2Cal.getTimeInMillis());
 
         timeBtwCal.setTimeInMillis(Math.abs(date1.getTime() - date2.getTime()));
 
@@ -35,21 +27,51 @@ public class DateDiff {
     }
 
     public static void timeToBirthday(Date now, Date birthday) {
-        now.setTime(System.currentTimeMillis());
+        Calendar zeroTime = Calendar.getInstance();
         Calendar nowTime = Calendar.getInstance();
         Calendar birthdayTime = Calendar.getInstance();
-// проверить, меньше ли месяц дня рождения текущего месяца. Если да (ДР уже прошёл), то YEAR++ в birthdayTime
-// если месяц совпадает, проверить, меньше ли день ДР, чем текущий день месяца (ДР уже прошёл), то YEAR++ в birthdayTime
-if (nowTime.get(Calendar.MONTH) >= birthdayTime.get(Calendar.MONTH) && nowTime.get(Calendar.DAY_OF_MONTH) > birthdayTime.get(Calendar.DAY_OF_MONTH)){
-//    YEAR++ в birthdayTime
-}
-        birthdayTime.set(Calendar.YEAR ,nowTime.get(Calendar.YEAR));
 
+        zeroTime.clear();
+        nowTime.clear();
+        birthdayTime.clear();
+
+        nowTime.setTime(now);
+        birthdayTime.setTime(birthday);
+//        birthdayTime.setTime(birthday);
+        birthdayTime.set(Calendar.YEAR, nowTime.get(Calendar.YEAR));
+        birthdayTime.set(Calendar.MONTH, birthdayTime.get(Calendar.MONTH));
+        birthdayTime.set(Calendar.DAY_OF_MONTH, birthdayTime.get(Calendar.DAY_OF_MONTH));
+// проверить, меньше ли месяц дня рождения текущего месяца. Если да (месяц ДР уже прошёл), и,
+// если месяц совпадает, проверить, меньше ли день ДР, чем текущий день месяца (ДР уже прошёл), то YEAR++ в birthdayTime
+        if (nowTime.get(Calendar.MONTH) >= birthdayTime.get(Calendar.MONTH) && nowTime.get(Calendar.DAY_OF_MONTH) > birthdayTime.get(Calendar.DAY_OF_MONTH)) {
+//    YEAR++ в birthdayTime
+            birthdayTime.set(Calendar.YEAR, nowTime.get(Calendar.YEAR) + 1);
+        }
+
+        Date timeToNextBD = new Date(birthdayTime.getTimeInMillis() - nowTime.getTimeInMillis());
+        Calendar timeToNextBdCal = Calendar.getInstance();
+        timeToNextBdCal.setTimeInMillis(timeToNextBD.getTime());
+
+        int monthDiff = Math.abs(timeToNextBdCal.get(Calendar.MONTH) - zeroTime.get(Calendar.MONTH));
+        int daysDiff = Math.abs(timeToNextBdCal.get(Calendar.DAY_OF_MONTH) - zeroTime.get(Calendar.DAY_OF_MONTH));
+        int hoursDiff = Math.abs(timeToNextBdCal.get(Calendar.HOUR) - zeroTime.get(Calendar.HOUR));
+        int minutesDiff = Math.abs(timeToNextBdCal.get(Calendar.MINUTE) - zeroTime.get(Calendar.MINUTE)) + hoursDiff * 60;
+        int secondsDiff = Math.abs(timeToNextBdCal.get(Calendar.SECOND) - zeroTime.get(Calendar.SECOND));
+        int millisDiff = Math.abs(timeToNextBdCal.get(Calendar.MILLISECOND) - zeroTime.get(Calendar.MILLISECOND));
+
+        System.out.println("До дня рождения " + monthDiff + " месяцев, " + daysDiff
+                + " дней, " + minutesDiff + " минут, " + secondsDiff + " секунд, " + millisDiff + " миллисекунд");
     }
 
     public static void main(String[] args) {
         Date d1 = new Date(10800000);
         Date d2 = new Date(31554000000L);
         timeBetween(d1, d2);
+        Date currentTime = new Date(System.currentTimeMillis());
+        Calendar myBirthday = Calendar.getInstance();
+        myBirthday.clear();
+        myBirthday.set(1973, 8, 14);
+        Date myBirthdayDate = new Date(myBirthday.getTimeInMillis());
+        timeToBirthday(currentTime, myBirthdayDate);
     }
 }
