79994beed9545fcca0b84f83c86ee284eae4acaf
smartglobus <smartglobus@gmail.com>
2022-11-21 15:33:53 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/tokenizer/JavaFormatter.java
+++ src/ru/progwards/java1/lessons/tokenizer/JavaFormatter.java
@@ -7,9 +7,7 @@ public class JavaFormatter {
 
     public static String format(String code) {
 
-
 // раскладываем строку на лексемы и все возможные знаки, включая пробелы (чтобы не потерять оригинальный формат фраз в кавычках)
-
         StringTokenizer rawString = new StringTokenizer(code, " \t\n\r\f.,!?|[]{}()\";:+-/*%=<>", true);
         // представляем строку как массив отдельных слов и символов
         int strElementsNum = rawString.countTokens();
@@ -17,91 +15,93 @@ public class JavaFormatter {
         Arrays.fill(rawCodeStr, ""); // предварительное заполнение массива, чтобы избежать значения null в пустых строках
         for (int j = 0; j < strElementsNum; j++) {
             rawCodeStr[j] = rawString.nextToken();
-//            System.out.println(rawCodeStr[j]);
         }
 
         String codeStringFormated = singleStringFormat(rawCodeStr);
-//        System.out.println(codeStringFormated);
 
-        StringTokenizer secondTake = new StringTokenizer(codeStringFormated, "\n{};", true);
-        int finalStringElementsNum = secondTake.countTokens();
-        String[] finalString = new String[finalStringElementsNum];
-        Arrays.fill(finalString, "");
-        for (int i = 0; i < finalStringElementsNum; i++) {
-            finalString[i] = secondTake.nextToken();
+        StringTokenizer secondPace = new StringTokenizer(codeStringFormated, "\n{};", true);
+        int curlyBracketsFormatedStringElementsNum = secondPace.countTokens();
+        String[] curlyBracketsFormatedString = new String[curlyBracketsFormatedStringElementsNum];
+        Arrays.fill(curlyBracketsFormatedString, "");
+        for (int i = 0; i < curlyBracketsFormatedStringElementsNum; i++) {
+            curlyBracketsFormatedString[i] = secondPace.nextToken();
         }
 
         // форматирование фигурных скобок
-        for (int i = 2; i < finalStringElementsNum; i++) {
+        for (int i = 2; i < curlyBracketsFormatedStringElementsNum; i++) {
 
             // форматирование открывающих фигурных скобок
-            if (finalString[i - 1].equals("\n") && finalString[i].equals("{")) {
-                if (finalString[i - 1].equals("\n") && finalString[i].equals("{") && finalString[i + 1].equals("\n")) {
+            if (curlyBracketsFormatedString[i - 1].equals("\n") && curlyBracketsFormatedString[i].equals("{")) {
+                if (curlyBracketsFormatedString[i - 1].equals("\n") && curlyBracketsFormatedString[i].equals("{") && curlyBracketsFormatedString[i + 1].equals("\n")) {
 
-                    finalString[i - 1] = "";
+                    curlyBracketsFormatedString[i - 1] = "";
                     continue;
                 } else {
 
-                    finalString[i - 1] = "{";
-                    finalString[i] = "\n";
+                    curlyBracketsFormatedString[i - 1] = "{";
+                    curlyBracketsFormatedString[i] = "\n";
                     continue;
                 }
             }
 
 
             // форматирование закрывающих фигурных скобок
-            if (finalString[i - 1].equals(";") && finalString[i].equals("}")) {
-                finalString[i - 1] = ";\n";
+            if (curlyBracketsFormatedString[i - 1].equals(";") && curlyBracketsFormatedString[i].equals("}")) {
+                curlyBracketsFormatedString[i - 1] = ";\n";
                 i++;
-                while (i < finalStringElementsNum && finalString[i].equals("}")) {
-                    finalString[i] = "\n}";
+                while (i < curlyBracketsFormatedStringElementsNum && curlyBracketsFormatedString[i].equals("}")) {
+                    curlyBracketsFormatedString[i] = "\n}";
                     i++;
                 }
             }
         }
-
-        for (String a : finalString) {
-            System.out.print(a);
+        // переводим в строку отформатированный по всем параметрам, кроме начальных отступов, код
+        // (иначе из-за оставшихся артефактов, типа "\n}", затруднительно провести форматирование отступов по '{', '}' и '\n'
+        String curlyBracketsFormated = "";
+        for (String a : curlyBracketsFormatedString) {
+            curlyBracketsFormated += a;
+        }
+//        System.out.println("----------");
+//        System.out.println(curlyBracketsFormated);
+//        System.out.println("----------");
+        StringTokenizer thirdPace = new StringTokenizer(curlyBracketsFormated, "\n{}", true);
+        int finalyFormatedStringElementsNum = thirdPace.countTokens();
+        //последний, "подсадной" элемент (+1) для корректной работы tabsFormat (обработка str[i+1]). Там в рабочем цикле он игнорируется
+        String[] finalyFormatedString = new String[finalyFormatedStringElementsNum + 1];
+        Arrays.fill(finalyFormatedString, "");
+        for (int i = 0; i < finalyFormatedStringElementsNum; i++) {
+            finalyFormatedString[i] = thirdPace.nextToken();
         }
 
-
-        return "тестируем пробелы \n ещё один \n  табуляция, отсюда\tдосюда";
+        return tabsFormat(finalyFormatedString);
     }
 
 
     public static String tabsFormat(String[] str) {
         int curlyBracketsCount = 0;
-
-        String result = str[0];
-        String lastLex = "";
+        String result = "";
         int strLength = str.length;
-        for (int i = 0; i < strLength; i++) {
-
+        for (int i = 0; i < strLength - 1; i++) {
 
             // считаем фигурные скобки
             if (str[i].equals("{")) {
                 curlyBracketsCount++;
             }
-            if (str[i].equals("}")) {
+            if (str[i + 1].equals("}")) {
                 curlyBracketsCount--;
             }
 
-
-            if (lastLex.equals("\n") || i == 0) {
+            if (str[i].equals("\n")) {
 
                 for (int j = 0; j < curlyBracketsCount; j++) {
-                    result += "    ";
+                    str[i] += "    ";
                 }
-                // заолняем строку, пока не встретится абзац или фигурная скобка
-                do {
-                    i++;
-                    result += str[i];
-                    lastLex = str[i];
-
-                } while (!str[i].equals("\n") || !str[i].equals("{") || !str[i].equals("}"));
-
+                result += str[i];
+            } else {
+                result += str[i];
             }
         }
+
         return result;
     }
 
@@ -212,7 +212,6 @@ public class JavaFormatter {
         return result;
     }
 
-
     static boolean isOperator(String lex) {
         String keyWordList = " abstract assert boolean break byte case catch char class const continue default do " +
                 " double else enum extends final finally float for goto if implements import instanceof int interface " +
@@ -277,7 +276,7 @@ public class JavaFormatter {
                 "            System.out.print( column + \"    \") ;\n" +
                 "        }\n" +
                 "     System.out.println ();    }}";
-        format(code);
 
+        System.out.println(format(code));
     }
 }
