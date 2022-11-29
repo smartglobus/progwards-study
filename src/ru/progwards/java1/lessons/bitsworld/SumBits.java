034e67971ecb533a2fbcff5527a2530926cd845d
smartglobus <smartglobus@gmail.com>
2022-11-29 21:29:06 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -4,17 +4,17 @@ public class SumBits {
     public static int sumBits(byte value) {
         int result = 0;
         byte denominator = 1;
-        for (int i = 0; i < 7; i++) {
-            if ((value & denominator) > 0) {
+        for (int i = 0; i < 8; i++) {
+
+            if ((value & denominator) != 0) {
                 result++;
             }
             denominator <<= 1;
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte)127));
+        System.out.println(sumBits((byte)0b10000000));
     }
 }
