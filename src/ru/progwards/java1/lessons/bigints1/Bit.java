84222d5a42c36fe7043b32b29a7a19c7ab22cc9c
smartglobus <smartglobus@gmail.com>
2022-12-05 23:35:37 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/bigints1/Bit.java
+++ src/ru/progwards/java1/lessons/bigints1/Bit.java
@@ -1,7 +1,5 @@
 package ru.progwards.java1.lessons.bigints1;
 
-import java.util.Arrays;
-
 public class Bit {
     boolean value;
 
@@ -18,21 +16,48 @@ public class Bit {
     }
 
 
-    class ByteRegister extends Bit {
+    public static class ByteRegister extends Bit {
         Bit[] eightBits = new Bit[8];
 
         public ByteRegister() {
-            Arrays.fill(eightBits, new Bit(false));
+            for (int i = 0; i < 8; i++) {
+                eightBits[i] = new Bit(false);
+            }
         }
 
         public ByteRegister(byte value) {
+            this();
             for (int i = 0, j = 1; i < 8; i++, j <<= 1) {
-                if ((value & j) == 1){
+                if ((value & j) != 0) {
                     eightBits[i].value = true;
                 }
             }
         }
 
+        @Override
+        public String toString() {
+            String result = "";
+            for (Bit bit : eightBits) {
+                result = bit.toString() + result;
+            }
+            return result;
+        }
 
+        public String toDecString() {
+            int result = eightBits[7].value ? 1 : 0;
+            for (int i = 6; i >=0; i--) {
+result = result*2 + (eightBits[i].value ? 1 : 0);
+            }
+
+            return Integer.toString(result);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Bit(true));
+        System.out.println(new ByteRegister((byte) 29));
+        System.out.println(new ByteRegister((byte) 29).toDecString());
     }
 }
