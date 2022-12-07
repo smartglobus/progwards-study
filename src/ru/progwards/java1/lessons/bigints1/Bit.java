5373bad1c6a8a88dfb4b3d9f6d29d9687e4b66f0
smartglobus <smartglobus@gmail.com>
2022-12-08 00:52:19 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/bigints1/Bit.java
+++ src/ru/progwards/java1/lessons/bigints1/Bit.java
@@ -84,24 +84,44 @@ public class Bit {
                 value.eightBits[i].value = value.eightBits[i + 1].value;
             }
             value.eightBits[7].value = true;
+        }
+    }
 
+    public static class ByteSummator {
+        public static boolean add(ByteRegister value1, ByteRegister value2) {
+            boolean isSumTrue = Integer.valueOf(value1.toDecString()) + Integer.valueOf(value2.toDecString()) <= 255;
+            boolean a;
+            boolean b;
+            boolean addOn = false;
+            boolean res;
+
+            for (int i = 0; i < 8; i++) {
+                a = value1.eightBits[i].value;
+                b = value2.eightBits[i].value;
+                res = a ^ b ^ addOn;
+                addOn = a & b || a & addOn || b & addOn;
+                value1.eightBits[i].value = res;
+            }
+            return isSumTrue;
         }
     }
 
     public static void main(String[] args) {
 //        System.out.println(new Bit(true));
 //        System.out.println(new ByteRegister((byte) 255));
-        ByteRegister a = new ByteRegister((byte) 0);
+        ByteRegister a = new ByteRegister((byte) 21);
         System.out.println(a.toDecString());
         System.out.println(a);
-        ByteCounter.dec(a);
+//        ByteCounter.dec(a);
         System.out.println(a.toDecString());
         System.out.println(a);
         ByteRegister b = new ByteRegister((byte) 234);
         System.out.println(b);
-        ByteShiftRegister.right(b);
+//        ByteShiftRegister.right(b);
         System.out.println(b.toDecString());
         System.out.println(b);
+        System.out.println(ByteSummator.add(a, b));
+
 
 //        System.out.println(Integer.toBinaryString(232));
 //        System.out.println(Integer.toBinaryString(231));
