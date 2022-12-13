228a2721900e15aeb4deab8ff721a2091b29ea39
smartglobus <smartglobus@gmail.com>
2022-12-13 15:03:38 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/egts/EgtsDirectionAndSpeed.java
+++ src/ru/progwards/java1/lessons/egts/EgtsDirectionAndSpeed.java
@@ -11,7 +11,7 @@ public class EgtsDirectionAndSpeed {
     }
 
     public static int getDirection(byte dirLow, short speedAndDir) {
-        int ninthBit = (speedAndDir & 0b00000000_00000000_10000000_00000000) >> 8;
+        int ninthBit = (speedAndDir & 0b00000000_00000000_10000000_00000000) >> 7;
         int eighthBit = dirLow & 0b00000000_00000000_00000000_10000000;
         int firstSevenBits = dirLow & 0b00000000_00000000_00000000_01111111;
 
@@ -20,7 +20,7 @@ public class EgtsDirectionAndSpeed {
 
     public static void main(String[] args) {
 
-        System.out.println(getDirection((byte) 0b10001010, (short) 0b10011100_00111010));
+        System.out.println(getDirection((byte) 0b11100111, (short) 0b00011100_00111010));
         System.out.println(getSpeed((short) 0b10011100_00111010));
 
     }
