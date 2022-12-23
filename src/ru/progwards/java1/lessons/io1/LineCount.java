9e0518f74fe107fdba35e1176d7757bc51f0df43
smartglobus <smartglobus@gmail.com>
2022-12-23 16:23:43 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -0,0 +1,27 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LineCount {
+    public static int calcEmpty(String fileName){
+        int emptyLine = 0;
+try{
+    FileReader reader = new FileReader(fileName);
+    Scanner scanner = new Scanner(reader);
+    try{
+        while (scanner.hasNextLine()){
+            if (scanner.nextLine().isEmpty()){
+                emptyLine++;
+            }
+        }
+    }finally {
+        scanner.close();
+    }
+}catch (IOException e){
+    return  -1;
+}
+        return emptyLine;
+    }
+}
