e61a85247b4998a4214edba189a782cb67718416
smartglobus <smartglobus@gmail.com>
2023-01-07 20:39:26 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -1,11 +1,64 @@
 package ru.progwards.java1.lessons.io2;
 
+import java.io.*;
+
 public class Censor {
 
-    public static void censorFile(String inoutFileName, String[] obscene){
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
 
+        try (RandomAccessFile censor = new RandomAccessFile(inoutFileName, "rw")) {
+
+            for (long i = 0; i < censor.length(); ) {
+                long currentLinePointer = censor.getFilePointer();// запоминаем положение курсора на начало текущей строки
+                String currentLine = censor.readLine();// читаем строку исходного файла
+                long nextLinePointer = censor.getFilePointer();// запоминаем положение курсора на начало следущей строки
+
+    // для каждого слова проверяем, есть ли оно в массиве obscene
+    // если слово подлежит цензуре, ставим курсор на позицию (начало строки + место найденного слова в этой строке)
+    // и перезаписываем звездочки в кол-ве, равном кол-ву букв в этом слове
+                String[] currLineArr = currentLine.split("[ ,.!\"?#()]");//разбиваем строку на слова по пробелам и др.
+                for (int j = 0; j < currLineArr.length; j++) {
+                    for (String s : obscene) {
+                        if (currLineArr[j].equals(s)) {
+                            censor.seek(currentLinePointer + currentLine.indexOf(currLineArr[j]));
+                            for (int z = 0; z < currLineArr[j].length(); z++) {
+                                censor.write('*');
+                            }
+                        }
+                    }
+                }
+                i = nextLinePointer;
+            }
+        } catch (Exception e) {
+            throw new CensorException(e.getMessage(), inoutFileName);
+        }
     }
 
+
+    static class CensorException extends Exception {
+        String message;
+        String fileName;
+
+        CensorException(String message, String fileName) {
+            super(message);
+            this.fileName = fileName;
+        }
+
+        @Override
+        public String toString() {
+            return fileName + ":" + message;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] obs = {"Java", "Oracle", "Sun", "Microsystems"};
+        try {
+            censorFile("C:\\Users\\User\\IdeaProjects\\Progwards first project\\src\\ru\\progwards\\java1\\lessons\\io2\\inoutFile.txt",
+                    obs);
+        } catch (CensorException e) {
+            e.printStackTrace();
+        }
+    }
 }
 /*
 Создать статический метод public static void censorFile(String inoutFileName, String[] obscene),
