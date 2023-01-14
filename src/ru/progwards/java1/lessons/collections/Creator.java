4c9c9b62613de35f6d95d6a11be7e01bf5be037c
smartglobus <smartglobus@gmail.com>
2023-01-14 23:16:04 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -0,0 +1,56 @@
+package ru.progwards.java1.lessons.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Creator {
+    public static Collection<Integer> fillEven(int n) {
+        Collection<Integer> list = new ArrayList<>();
+        int lim = n * 2;
+        for (int i = 2; i <= lim; i += 2)
+            list.add(i);
+        return list;
+    }
+
+    public static Collection<Integer> fillOdd(int n) {
+        List<Integer> list = new ArrayList<>();
+        int lim = n * 2;
+
+//        for (int i = lim; i > 0; i -= 2)
+        for (int i = 1; i < lim; i += 2)
+            list.add(0, i);
+        return list;
+    }
+
+    public static Collection<Integer> fill3(int n) {
+        Collection<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+            list.add(i * i);
+            list.add(i * i * i);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        for (Integer i : fillEven(6))
+            System.out.println(i);
+        System.out.println("");
+        for (Integer i : fillOdd(6))
+            System.out.println(i);
+        System.out.println("");
+        for (Integer i : fill3(6))
+            System.out.println(i);
+    }
+}
+//создать коллекцию и заполнить последовательностью четных возрастающих чисел начиная с 2, количество элементов в коллекции n
+//создать коллекцию и заполнить последовательностью нечетных убывающих чисел, минимальное число в коллекции 1, количество элементов в коллекции n
+//создать коллекцию и заполнить ее тройками чисел. Каждая тройка создается по алгоритму:
+// первое число тройки - индекс числа в коллекции,
+// второе - тот же индекс в квадрате,
+// третье - тот же индекс в кубе,
+// количество элементов в коллекции n*3
