8f13b2f7150aa859eb8feeaa53fbca8f5599170d
smartglobus <smartglobus@gmail.com>
2023-01-21 17:34:53 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -1,4 +1,48 @@
 package ru.progwards.java1.lessons.collections;
 
-public class MatrixIterator {
+import java.util.Iterator;
+
+public class MatrixIterator<T> implements Iterator<T> {
+
+    private T[][] array;
+
+    MatrixIterator(T[][] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
 }
+/*
+Сделать итератор по одномерному массиву, реализовать методы hasNext() и next(). Шаблон пустого итератора:
+
+	public class ArrayIterator<T> implements Iterator<T> {
+
+		private T[] array;
+
+		ArrayIterator(T[] array) {
+			this.array = array;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public T next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
+
+
+ */
