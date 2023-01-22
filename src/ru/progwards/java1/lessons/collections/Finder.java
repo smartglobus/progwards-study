4c7981a595f0d46e4996fbc92fe6244d498af2d4
smartglobus <smartglobus@gmail.com>
2023-01-22 22:39:58 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -15,7 +15,7 @@ public class Finder {
         int numNxtCurrent = iterFromOne.next();
         int sumResult = numCurrent + numNxtCurrent;
         int numIndex = 0;
-        int numNxtIndex = 0;
+        int numNxtIndex = 1;
 
         while (iterFromOne.hasNext()) {
             numCurrent = iterFromZero.next();
@@ -27,8 +27,8 @@ public class Finder {
                 numNxtIndex = iterFromOne.nextIndex() - 1;
             }
         }
-        twoInd.add(0, numIndex);
-        twoInd.add(1, numNxtIndex);
+        twoInd.add(numIndex);
+        twoInd.add(numNxtIndex);
 
         return twoInd;
     }
@@ -39,18 +39,18 @@ public class Finder {
         List<Integer> nums = new ArrayList<>(numbers);
         List<Integer> lokalMaxRecord = new ArrayList<>();
         ListIterator<Integer> iterator = nums.listIterator();
-        int last= iterator.next();
-        int current= iterator.next();
-        int next = iterator.next();
+        int last = iterator.next();
+        int current = iterator.next();
 
         while (iterator.hasNext()) {
 
+            int next = iterator.next();
             if (current > next && current > last) {
                 lokalMaxRecord.add(current);
             }
             last = current;
             current = next;
-            next = iterator.next();
+
         }
         return lokalMaxRecord;
     }
@@ -94,7 +94,7 @@ public class Finder {
 
         List<Integer> nt = new ArrayList<>(Arrays.asList(7, 4, 9, 54, -5, 9, 25, 24, 45, 8));
         List<Integer> oneTwoThree = new ArrayList<>(Arrays.asList(1, 3, 2, 5, 4));
-        List<Integer> nt2 = new ArrayList<>(Arrays.asList(98, 52, -14, -62, 57, -40, 8));
+        List<Integer> nt2 = new ArrayList<>(Arrays.asList(67,113,-40,-53,94,12,-21,39,-78,40,-53));
 
         for (Integer value : nt2) System.out.print(value + " ");
         System.out.println("\n");
@@ -107,7 +107,7 @@ public class Finder {
         System.out.println("\n");
         List<String> names = new ArrayList<>(Arrays.asList("Fedor", "Masha", "Masha", "Petya", "Petya", "Masha", "Masha", "Masha", "Vasya", "Petya", "Masha", "Masha", "Masha", "Masha", "Vasya", "Vasya", "Vasya", "Vasya", "Petya"));
         System.out.println(findSimilar(names) + "\n");
-        List<String> names1 = new ArrayList<>(Arrays.asList("Fedor", "Masha", "Petya", "Masha",  "Vasya"));
+        List<String> names1 = new ArrayList<>(Arrays.asList("Fedor", "Masha", "Petya", "Masha", "Vasya"));
         System.out.println(findSimilar(names1) + "\n");
     }
 }
