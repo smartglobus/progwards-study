9cda31573d24fbfd7cd66433d7fd207fc05af86a
smartglobus <smartglobus@gmail.com>
2023-01-29 22:30:49 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -21,7 +21,7 @@ class Shop {
     private List<Product> products;
 
     public Shop(List<Product> products) {
-        this.products = products;
+        this.products = new ArrayList<>(products);
     }
 
     public List<Product> getProducts() {
