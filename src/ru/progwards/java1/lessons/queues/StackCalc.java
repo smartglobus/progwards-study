4a9126ede44228bb18ddf82a25057414e2f67137
smartglobus <smartglobus@gmail.com>
2023-02-13 00:49:23 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -1,4 +1,37 @@
 package ru.progwards.java1.lessons.queues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StackCalc {
+    Deque<Double> stack = new ArrayDeque<>();
+
+    public void push(double value){
+        stack.push(value);
+    }
+
+    public double pop(){
+        return stack.pop();
+    }
+
+// сложить 2 верхних значения на стеке, результат положить на стек. В итогу в стеке должно быть на один элемент меньше
+    public void add(){
+        stack.push(stack.pop() + stack.pop());
+    }
+
+// вычесть верхнее значение на стеке, из следующего по глубине, результат положить на стек. В итоге в стеке должно быть на один элемент меньше
+    public void sub(){
+        stack.push(-stack.pop() + stack.pop());
+    }
+
+// умножить 2 верхних значения на стеке, результат положить на стек. В итогу в стеке должно быть на один элемент меньше
+    public void mul() {
+        stack.push(stack.pop() * stack.pop());
+    }
+
+// поделить на верхнее значение на стеке, следующее по глубине, результат положить на стек. В итоге в стеке должно быть на один элемент меньше
+    public void div(){
+        stack.push(stack.pop() / stack.pop());
+    }
+
 }
