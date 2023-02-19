bcd636d316ed1df3055518abb3eb7cb3e96ec6e7
smartglobus <smartglobus@gmail.com>
2023-02-19 20:29:38 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -1,8 +1,6 @@
 package ru.progwards.java1.lessons.maps;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -12,14 +10,16 @@ public class FiboMapCache {
 
     public FiboMapCache(boolean cacheOn) {
         this.cacheOn = cacheOn;
-        this.fiboCache = new HashMap<>();
+        this.fiboCache = new Hashtable<>();
     }
 
     //в функции проверить, находится ли вычисленное значение для n в кэше, и если да - вернуть его из кэша, если нет - рассчитать и добавить в кэш.
 // Учитывать значение переменной cacheOn
     public BigDecimal fiboNumber(int n) {
-        if (cacheOn && fiboCache.containsKey(n)) return fiboCache.put(n, fibonacci(n));
-        return fiboCache.put(n, fibonacci(n));
+        if (cacheOn && fiboCache.containsKey(n)) return fiboCache.get(n);
+
+        fiboCache.put(n, fibonacci(n));
+        return fiboCache.get(n);
     }
 
     public static BigDecimal fibonacci(int n) {
@@ -44,29 +44,28 @@ public class FiboMapCache {
     //тест для расчета чисел Фибоначчи от n = 1 до 1000 включительно и замерить разницу во времени с on = true и on = false,
 // результат вывести на экран в формате "fiboNumber cacheOn=??? время выполнения ???" для cacheOn=true и cacheOn=false,
 // вместо ??? вывести реальные значения в мсек.
-    public void test() {
-//        FiboMapCache fmcTest = new FiboMapCache(false);
+    public static void test() {
+        FiboMapCache fmcTest = new FiboMapCache(false);
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 1000; i++) {
-            fiboNumber(i);
+            fmcTest.fiboNumber(i);
         }
-        System.out.println("fiboNumber cacheOn=" + cacheOn + " время выполнения " + (System.currentTimeMillis() - startTime));
-//        fmcTest.clearCahe();
-//        fmcTest.cacheOn = true;
-//
-//        startTime = System.currentTimeMillis();
-//        for (int i = 0; i < 1000; i++) {
-//            fmcTest.fiboNumber(i);
-//        }
-//        System.out.println("fiboNumber cacheOn=" + fmcTest.cacheOn + " время выполнения " + (System.currentTimeMillis() - startTime));
+        System.out.println("fiboNumber cacheOn=" + fmcTest.cacheOn + " время выполнения " + (System.currentTimeMillis() - startTime));
+
+        fmcTest.cacheOn = true;
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            fmcTest.fiboNumber(i);
+        }
+        System.out.println("fiboNumber cacheOn=" + fmcTest.cacheOn + " время выполнения " + (System.currentTimeMillis() - startTime));
     }
 
     public static void main(String[] args) {
-//        FiboMapCache fmcTestFalse = new FiboMapCache(false);
-//        fmcTestFalse.test();
-//        fmcTestFalse.clearCahe();
-        FiboMapCache fmcTestTrue = new FiboMapCache(true);
-        fmcTestTrue.test();
-        System.out.println(fmcTestTrue.fiboCache.get(8));
+        FiboMapCache.test();
+//        FiboMapCache fmcTestTrue = new FiboMapCache(true);
+//        System.out.println(fmcTestTrue.fiboNumber(8));
+//        System.out.println(fmcTestTrue.fiboCache.get(8));
+
     }
 }
