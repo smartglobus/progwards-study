aec0d3edbb323957c8b8a8f018ad24c79c7bd2bc
smartglobus <smartglobus@gmail.com>
2023-02-21 21:09:10 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/maps/UsageFrequency.java
+++ src/ru/progwards/java1/lessons/maps/UsageFrequency.java
@@ -1,17 +1,23 @@
 package ru.progwards.java1.lessons.maps;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class UsageFrequency {
     private String fileToString;
 
     public void processFile(String fileName) {
-        try (FileReader reader = new FileReader(fileName); Scanner scanner = new Scanner(reader)) {
-            StringBuilder builder = new StringBuilder();
-            while (scanner.hasNextLine()) builder.append(scanner.nextLine());
-            fileToString = builder.toString();
+
+        try {//(FileReader reader = new FileReader(fileName); Scanner scanner = new Scanner(reader)) {
+            fileToString = FileUtils.readFileToString(new File(fileName), StandardCharsets.UTF_8);
+//            StringBuilder builder = new StringBuilder();
+//            while (scanner.hasNextLine()) builder.append(scanner.nextLine());
+//            fileToString = builder.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,18 +38,14 @@ public class UsageFrequency {
     }
 
 
-    // вернуть Map, который содержит все найденные слова и количество раз, которое каждое слово встретилось.
+// вернуть Map, который содержит все найденные слова и количество раз, которое каждое слово встретилось.
 // Знаки препинания, такие как “.,!? @” и др являются разделителями.
     public Map<String, Integer> getWords() {
         Map<String, Integer> getWrd = new HashMap<>();
-//        fileToString.replace(""," ");
         String[] words = fileToString.split("[\\W]+");
         List<String> wordsSet = new ArrayList<>(Arrays.asList(words));
         for (int i = 0; i < wordsSet.size(); i++) {
-            if ("".equals(wordsSet.get(i)) || wordsSet.get(i) == null) {
-//                wordsSet.remove(i);
-                continue;
-            }
+            if ("".equals(wordsSet.get(i)) || wordsSet.get(i) == null) continue;
             getWrd.putIfAbsent(wordsSet.get(i), Collections.frequency(wordsSet, wordsSet.get(i)));
         }
         return getWrd;
