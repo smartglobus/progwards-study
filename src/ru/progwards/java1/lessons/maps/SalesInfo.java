779703b26ba91c2b11519dfe932df66949059488
smartglobus <smartglobus@gmail.com>
2023-02-27 22:56:52 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -1,22 +1,42 @@
 package ru.progwards.java1.lessons.maps;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class SalesInfo {
     private List<String> buyersData = new ArrayList<>();
+    private List<Buy> buys = new ArrayList<>();
 
     //  вернуть количество успешно загруженных строк.
     //  Если в строке более или менее 4-x полей, или количество и сумма не преобразуются в числа - эту строку не загружаем.
+//    public int loadOrdersOld(String fileName) {
+//        int count = 0;
+//        try (FileReader reader = new FileReader(fileName); Scanner scanner = new Scanner(reader)) {
+//            while (scanner.hasNextLine()) {
+//                String currLine = scanner.nextLine();
+//                if (checkLine(currLine)) {
+//                    buyersData.add(currLine);
+//                    count++;
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return count;
+//    }
+
     public int loadOrders(String fileName) {
         int count = 0;
         try (FileReader reader = new FileReader(fileName); Scanner scanner = new Scanner(reader)) {
             while (scanner.hasNextLine()) {
                 String currLine = scanner.nextLine();
-                if (checkLine(currLine)) {
-                    buyersData.add(currLine);
+                try {
+                    buys.add(new Buy(currLine));
                     count++;
+                } catch (Exception e) {
+//                    continue;
                 }
             }
         } catch (IOException e) {
@@ -43,12 +63,13 @@ public class SalesInfo {
         int qty;
         double sum;
 
-        Buy(String byr) {
+        Buy(String byr) throws Exception{
             String[] buyer = byr.split(",");
+            if (buyer.length != 4) throw new Exception();
             this.name = buyer[0].trim();
             this.good = buyer[1].trim();
-            this.qty = Integer.valueOf(buyer[2].trim());
-            this.sum = Double.valueOf(buyer[3].trim());
+            this.qty = Integer.parseInt(buyer[2].trim());
+            this.sum = Double.parseDouble(buyer[3].trim());
         }
     }
 
@@ -57,10 +78,10 @@ public class SalesInfo {
     public Map<String, Double> getGoods() {
         Map<String, Double> getGoods = new TreeMap<>();
 
-        for (String b : buyersData) {
-            Buy currByr = new Buy(b);
-            if (getGoods.putIfAbsent(currByr.good, currByr.sum) != null)
-                getGoods.replace(currByr.good, getGoods.get(currByr.good) + currByr.sum);
+        for (Buy currBuy : buys) {
+//            Buy currBuy = new Buy(b);
+            if (getGoods.putIfAbsent(currBuy.good, currBuy.sum) != null)
+                getGoods.replace(currBuy.good, getGoods.get(currBuy.good) + currBuy.sum);
         }
         return getGoods;
     }
@@ -89,8 +110,8 @@ public class SalesInfo {
 
     public Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers() {
         Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers = new TreeMap<>();
-        for (String b : buyersData) {
-            Buy currBuy = new Buy(b);
+        for (Buy currBuy : buys) {
+//            Buy currBuy = new Buy(b);
             if (getCustomers.putIfAbsent(currBuy.name, new AbstractMap.SimpleEntry<>(currBuy.sum, currBuy.qty)) != null){
                 AbstractMap.SimpleEntry<Double,Integer> currEntry = getCustomers.get(currBuy.name);
                 currEntry.setValue(currEntry.getValue() + currBuy.qty);
@@ -103,7 +124,8 @@ public class SalesInfo {
 
     public static void main(String[] args) {
         SalesInfo test = new SalesInfo();
-        test.loadOrders("C:\\Users\\User\\IdeaProjects\\Progwards first project\\src\\ru\\progwards\\java1\\lessons\\maps\\Sales.csv");
+        System.out.println(test.loadOrders("C:\\Users\\User\\IdeaProjects\\Progwards first project\\src\\ru\\progwards\\java1\\lessons\\maps\\Sales.csv"));
+
         for (Map.Entry e : test.getGoods().entrySet()) {
             System.out.println(e);
         }
