48040bf82730727c87ce434338b4a48be0f7b50b
smartglobus <smartglobus@gmail.com>
2023-03-17 23:04:00 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -15,7 +15,7 @@ public class Profiler {
 //            allSections.get(name).firstEnterTime = enterTime;
         }
         allSections.get(name).lastEnterTime = enterTime;// нужно ли оно ЗДЕСЬ???
-
+        allSections.get(name).selfTimeCorrection = 0;
     }
 
     public static void exitSection(String name) {
@@ -23,14 +23,14 @@ public class Profiler {
 //        allSections.get(name).lastExitTime = exitTime;
         long lastDuration = exitTime - allSections.get(name).lastEnterTime;
         allSections.get(name).fullTime += (int) lastDuration;
-        allSections.get(name).selfTime += (int) (lastDuration);// нужно ли оно ЗДЕСЬ???
+        allSections.get(name).selfTime += (int) (lastDuration + allSections.get(name).selfTimeCorrection);// нужно ли оно ЗДЕСЬ???
         allSections.get(name).lastExitTime = exitTime;// нужно ли оно???
         allSections.get(name).count++;
 
         ArrayList<StatisticInfo> currSectionsList = new ArrayList<>(allSections.values());// текущий список уникальных секций
         for (StatisticInfo s : currSectionsList) {
             if (s.lastEnterTime > s.lastExitTime) { // если секция s ещё не завершилась
-                s.selfTime -= lastDuration;
+                s.selfTimeCorrection -= lastDuration;
             }
         }
 
@@ -72,6 +72,30 @@ public class Profiler {
 
     public static void main(String[] args) {
 
+        try {
+            for (int i = 0; i < 3; i++) {
+                enterSection("pr1");
+                Thread.sleep(100);
+                if (i == 2) {
+                    for (int j = 0; j < 3; j++) {
+                        enterSection("pr2");
+                        Thread.sleep(200);
+                        enterSection("pr3");
+                        Thread.sleep(100);
+                        exitSection("pr3");
+                        exitSection("pr2");
+                    }
+                }
+                exitSection("pr1");
+            }
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (StatisticInfo si : getStatisticInfo()) {
+            System.out.println(si.sectionName + "  " + si.fullTime + "  " + si.selfTime + "  " + si.count);
+        }
     }
 }
 
@@ -82,9 +106,10 @@ class StatisticInfo {
     public int selfTime;// - чистое время выполнения секции в миллисекундах. Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена выполнения вложенных секций.
     public int count;// - количество вызовов. В случае, если вызовов более одного, fullTime и selfTime содержат суммарное время выполнения всех вызовов.
 
-//    public long firstEnterTime;
+    //    public long firstEnterTime;
     public long lastEnterTime;
     public long lastExitTime;// нужно ли оно???
+    public long selfTimeCorrection;
 
 
 }
