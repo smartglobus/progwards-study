85cc52da8e091c85be025fcea4b35342544eda7d
smartglobus <smartglobus@gmail.com>
2023-03-18 01:12:58 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -5,61 +5,47 @@ import java.util.*;
 
 public class Profiler {
 
-    private static Map<String, StatisticInfo> allSections = new HashMap<>();// словарь (по сути, множество) из уникальных секций
-    //    private static SortedMap<Long, StatisticInfo> insAndOuts = new TreeMap<>();// сортированный по времени таймлайн со всеми временами первого входа и последнего выхода
-    private static ArrayList<StatisticInfo> sectionsList = new ArrayList<>();// текущий список уникальных секций
+    private static Map<String, StatisticInfo> allSections = new HashMap<>();  // словарь из уникальных секций
+    private static ArrayList<StatisticInfo> sectionsList = new ArrayList<>();  // текущий список уникальных секций
 
     public static void enterSection(String name) {
         long enterTime = Instant.now().toEpochMilli();
         if (allSections.putIfAbsent(name, new StatisticInfo()) == null) {
             allSections.get(name).sectionName = name;
-//            allSections.get(name).firstEnterTime = enterTime;
             sectionsList.add(allSections.get(name));
         }
-        allSections.get(name).lastEnterTime = enterTime;     //                      нужно ли оно ЗДЕСЬ???
-        allSections.get(name).selfTimeCorrection = 0;
-        System.out.println(allSections.get(name).sectionName +" open");
+        allSections.get(name).lastEnterTime = enterTime;
+        allSections.get(name).selfTimeCorrection = 0; // обнуление прошлой коррекции
     }
 
     public static void exitSection(String name) {
         long exitTime = Instant.now().toEpochMilli();
-//        allSections.get(name).lastExitTime = exitTime;
         long lastDuration = exitTime - allSections.get(name).lastEnterTime;
         allSections.get(name).fullTime += (int) lastDuration;
         allSections.get(name).selfTime += (int) (lastDuration + allSections.get(name).selfTimeCorrection);
-//        allSections.get(name).selfTimeCorrection = 0;
-        System.out.println(allSections.get(name).sectionName +" close   lastDuration = "+lastDuration+",  corr = " +allSections.get(name).selfTimeCorrection);
-
-        allSections.get(name).lastExitTime = exitTime;                                                                       // нужно ли оно???
+        allSections.get(name).lastExitTime = exitTime;
         allSections.get(name).count++;
 
-        // !!! надо не создавать новый список, а дополнять существующий при создании новой секции!!!
-
+        // внесение поправок в текущую коррекцию для будущего вычисления selfTime для текущих секций (то есть, для которых закрывающаяся "name" является вложенной)
         for (StatisticInfo s : sectionsList) {
-            if (s.lastEnterTime >= s.lastExitTime) { // если секция s ещё не завершилась
+            if (s.lastEnterTime >= s.lastExitTime) { // если секция "s" ещё не завершилась
                 s.selfTimeCorrection -= lastDuration + allSections.get(name).selfTimeCorrection;
             }
         }
-//        System.out.println(allSections.get("pr1").sectionName+"  corr = " +allSections.get("pr1").selfTimeCorrection);
     }
 
     public static List<StatisticInfo> getStatisticInfo() {
-
-
-//        ArrayList<StatisticInfo> sectionsList = new ArrayList<>(allSections.values());// список уникальных секций
         sectionsList.sort(new Comparator<StatisticInfo>() {
             @Override
             public int compare(StatisticInfo o1, StatisticInfo o2) {
                 return o1.sectionName.compareTo(o2.sectionName);
             }
         });
-
         return sectionsList;
     }
 
 
     public static void main(String[] args) {
-
         try {
             for (int i = 0; i < 3; i++) {
                 enterSection("pr1");
@@ -76,8 +62,6 @@ public class Profiler {
                 }
                 exitSection("pr1");
             }
-
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -88,16 +72,12 @@ public class Profiler {
 }
 
 class StatisticInfo {
+    public String sectionName;  // - имя секции
+    public int fullTime;  // - полное время выполнения секции в миллисекундах.
+    public int selfTime;  // - чистое время выполнения секции в миллисекундах. Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена выполнения вложенных секций.
+    public int count;  // - количество вызовов. В случае, если вызовов более одного, fullTime и selfTime содержат суммарное время выполнения всех вызовов.
 
-    public String sectionName;// - имя секции
-    public int fullTime;// - полное время выполнения секции в миллисекундах.
-    public int selfTime;// - чистое время выполнения секции в миллисекундах. Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена выполнения вложенных секций.
-    public int count;// - количество вызовов. В случае, если вызовов более одного, fullTime и selfTime содержат суммарное время выполнения всех вызовов.
-
-    //    public long firstEnterTime;
     public long lastEnterTime;
-    public long lastExitTime;// нужно ли оно???
+    public long lastExitTime;
     public long selfTimeCorrection;
-
-
 }
