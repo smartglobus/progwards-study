210d7cdc382645eb6f62bdddb9641ff9b0abe9b4
smartglobus <smartglobus@gmail.com>
2023-03-17 19:36:42 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -6,25 +6,34 @@ import java.util.*;
 public class Profiler {
 
     private static Map<String, StatisticInfo> allSections = new HashMap<>();// словарь (по сути, множество) из уникальных секций
-    private static SortedMap<Long, StatisticInfo> insAndOuts = new TreeMap<>();// сортированный по времени таймлайн со всеми временами первого входа и последнего выхода
+//    private static SortedMap<Long, StatisticInfo> insAndOuts = new TreeMap<>();// сортированный по времени таймлайн со всеми временами первого входа и последнего выхода
 
     public static void enterSection(String name) {
         long enterTime = Instant.now().toEpochMilli();
         if (allSections.putIfAbsent(name, new StatisticInfo()) == null) {
             allSections.get(name).sectionName = name;
-            allSections.get(name).firstEnterTime = enterTime;
+//            allSections.get(name).firstEnterTime = enterTime;
         }
-//        allSections.get(name).lastEnterTime = enterTime;// нужно ли оно ЗДЕСЬ???
+        allSections.get(name).lastEnterTime = enterTime;// нужно ли оно ЗДЕСЬ???
 
     }
 
     public static void exitSection(String name) {
         long exitTime = Instant.now().toEpochMilli();
 //        allSections.get(name).lastExitTime = exitTime;
-        allSections.get(name).fullTime = (int) (exitTime - allSections.get(name).firstEnterTime);
-//        allSections.get(name).selfTime += (int) (exitTime - allSections.get(name).lastEnterTime);// нужно ли оно ЗДЕСЬ???
-//        allSections.get(name).lastExitTime = exitTime;// нужно ли оно???
+        long lastDuration = exitTime - allSections.get(name).lastEnterTime;
+        allSections.get(name).fullTime += (int) lastDuration;
+        allSections.get(name).selfTime += (int) (lastDuration);// нужно ли оно ЗДЕСЬ???
+        allSections.get(name).lastExitTime = exitTime;// нужно ли оно???
         allSections.get(name).count++;
+
+        ArrayList<StatisticInfo> currSectionsList = new ArrayList<>(allSections.values());// текущий список уникальных секций
+        for (StatisticInfo s : currSectionsList) {
+            if (s.lastEnterTime > s.lastExitTime) { // если секция s ещё не завершилась
+                s.selfTime -= lastDuration;
+            }
+        }
+
     }
 
     public static List<StatisticInfo> getStatisticInfo() {
@@ -46,16 +55,16 @@ public class Profiler {
                 return o1.sectionName.compareTo(o2.sectionName);
             }
         });
-        for (StatisticInfo s : sectionsList) s.selfTime = s.fullTime;// предварительная инициализация selfTime
+//        for (StatisticInfo s : sectionsList) s.selfTime = s.fullTime;// предварительная инициализация selfTime
 
-        for (StatisticInfo sectionOfInterest : sectionsList) {
-            for (StatisticInfo s : sectionsList) {
-                //если секция s началась позже sectionOfInterest но закончилась раньше её окончания, значит секция s вложенная
-                if (s.firstEnterTime > sectionOfInterest.firstEnterTime && s.firstEnterTime < (sectionOfInterest.firstEnterTime + sectionOfInterest.fullTime)) {
-                    sectionOfInterest.selfTime -= s.fullTime;
-                }
-            }
-        }
+//        for (StatisticInfo sectionOfInterest : sectionsList) {
+//            for (StatisticInfo s : sectionsList) {
+//                //если секция s началась позже sectionOfInterest но закончилась раньше её окончания, значит секция s вложенная
+//                if (s.firstEnterTime > sectionOfInterest.firstEnterTime && s.firstEnterTime < sectionOfInterest.lastExitTime) {
+//                    sectionOfInterest.selfTime -= s.fullTime;
+//                }
+//            }
+//        }
 
         return sectionsList;
     }
@@ -73,9 +82,9 @@ class StatisticInfo {
     public int selfTime;// - чистое время выполнения секции в миллисекундах. Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена выполнения вложенных секций.
     public int count;// - количество вызовов. В случае, если вызовов более одного, fullTime и selfTime содержат суммарное время выполнения всех вызовов.
 
-    public long firstEnterTime;
-//    public long lastEnterTime;
-//    public long lastExitTime;// нужно ли оно???
+//    public long firstEnterTime;
+    public long lastEnterTime;
+    public long lastExitTime;// нужно ли оно???
 
 
 }
