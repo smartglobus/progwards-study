d55384b92efa7f5cbecb59ed6bc872ad5fa76723
smartglobus <smartglobus@gmail.com>
2023-05-06 19:34:00 +0300

    smartglobus study

---
+++ src/ru/progwards/java1/lessons/generics/DynamicArray.java
@@ -0,0 +1,72 @@
+package ru.progwards.java1.lessons.generics;
+
+import java.util.Arrays;
+
+public class DynamicArray<T> {
+    private T[] array;
+    private static int entryCount;
+
+    DynamicArray() {
+        array = (T[]) new Object[10];
+    }
+
+    void add(T t) {
+        if (entryCount > array.length - 1) {
+            T[] newArray = (T[]) new Object[array.length * 2];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array=newArray;
+        }
+        array[entryCount++] = t;
+    }
+
+    void insert(int pos, T t){
+        if (entryCount > array.length - 1) {
+            T[] newArray = (T[]) new Object[array.length * 2];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array=newArray;
+        }
+        for (int i = entryCount-1; i >= pos; i--) array[i + 1] = array[i];
+        array[pos]= t;
+        entryCount++;
+    }
+
+    void remove(int pos){
+        for (int i = pos; i < entryCount; i++) array[i] = array[i + 1];
+        array[entryCount]=null;
+        entryCount--;
+    }
+
+    T get(int pos){
+        return array[pos];
+    }
+
+    int size(){
+        return entryCount;
+    }
+
+
+
+    public static void main(String[] args) {
+        DynamicArray da = new DynamicArray();
+        da.add(2);
+        da.add("test");
+        da.add(12.8);
+        da.add(true);
+        da.insert(2,"insert");
+
+        Arrays.stream(da.array).forEach(System.out::println);
+        System.out.println("\n"+ "Dynarray size = "+ da.size()+ "\n");
+        da.remove(0);
+        Arrays.stream(da.array).forEach(System.out::println);
+        System.out.println("\n" + da.get(2)+ "\n");
+
+        da.add(222);
+        da.add("testing e");
+        da.add(125.25);
+        da.add(false);
+        da.add("testing exp");
+        da.add("testing expan");
+        da.add("testing expantion");
+        Arrays.stream(da.array).forEach(System.out::println);
+    }
+}
