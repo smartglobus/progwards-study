5cb8337c464ea5161c95316b1a9c9944182f7662
smartglobus <smartglobus@gmail.com>
2023-05-14 19:21:36 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/generics/ArraySort.java
+++ src/ru/progwards/java1/lessons/generics/ArraySort.java
@@ -2,12 +2,12 @@ package ru.progwards.java1.lessons.generics;
 
 import java.util.Arrays;
 
-public class ArraySort{
+public class ArraySort {
 
     public static <T extends Comparable<T>> void sort(T... arr) {
         for (int i = 0; i < arr.length - 1; i++)
             for (int j = i + 1; j < arr.length; j++)
-                if (arr[i].compareTo(arr[j]) == 1) {
+                if (arr[i].compareTo(arr[j]) >= 1) {
                     T temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
@@ -15,8 +15,9 @@ public class ArraySort{
     }
 
     public static void main(String[] args) {
-
         Integer[] testArr = new Integer[]{25, 6, 34, 87, 9, 43, 1};
+        Comparable[] fakearr= new Comparable[]{3,"abc",25.5f};
+//        sort(fakearr);
         sort(testArr);
         Arrays.stream(testArr).forEach(System.out::println);
     }
