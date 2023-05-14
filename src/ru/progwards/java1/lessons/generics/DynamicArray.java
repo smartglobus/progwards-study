c524f9d6645b2c1525368bd4f273042ceb21d099
smartglobus <smartglobus@gmail.com>
2023-05-14 13:53:48 +0300

    smartglobus study

--- src/ru/progwards/java1/lessons/generics/DynamicArray.java
+++ src/ru/progwards/java1/lessons/generics/DynamicArray.java
@@ -1,19 +1,16 @@
 package ru.progwards.java1.lessons.generics;
 
-
-import java.util.Arrays;
-
-public class DynamicArray<T extends Object> {
+public class DynamicArray<T> {
     private T[] array;
     private int entryCount;
 
-    //    @SuppressWarnings("unchecked")
-     DynamicArray() {
+    @SuppressWarnings("unchecked")
+    DynamicArray() {
         array = (T[]) new Object[1];
     }
 
-    //    @SuppressWarnings("unchecked")
-     void add(T t) {
+    @SuppressWarnings("unchecked")
+    void add(T t) {
         if (entryCount >= array.length) {
             T[] newArray = (T[]) new Object[array.length * 2];
             System.arraycopy(array, 0, newArray, 0, array.length);
@@ -22,8 +19,8 @@ public class DynamicArray<T extends Object> {
         array[entryCount++] = t;
     }
 
-    //    @SuppressWarnings("unchecked")
-     void insert(int pos, T t) {
+    @SuppressWarnings("unchecked")
+    void insert(int pos, T t) {
         if (pos > array.length - 1) return;
         if (entryCount >= array.length) {
             T[] newArray = (T[]) new Object[array.length * 2];
@@ -35,7 +32,7 @@ public class DynamicArray<T extends Object> {
         entryCount++;
     }
 
-     void remove(int pos) {
+    void remove(int pos) {
         if (pos >= entryCount) return;
         System.arraycopy(array, pos + 1, array, pos, entryCount - 1 - pos);
         array[entryCount - 1] = null;
@@ -47,28 +44,27 @@ public class DynamicArray<T extends Object> {
         return array[pos];
     }
 
-     int size() {
+    int size() {
         return entryCount;
     }
 
 
     public static void main(String[] args) {
-        DynamicArray da = new DynamicArray<>();
+        DynamicArray<Integer> da = new DynamicArray<>();
         da.add(1);
         da.add(2);
         da.add(3);
         da.add(222);
-        Arrays.stream(da.array).forEach(System.out::println);
+        for (int i = 0; i < da.size(); i++) System.out.println(da.get(i));
 
         System.out.println("\nentryCount = " + da.entryCount + "\n");
 //        da.insert(0, 5);
 //        da.insert(2, 4);
 
-        Arrays.stream(da.array).forEach(System.out::println);
+        for (int i = 0; i < da.size(); i++) System.out.println(da.get(i));
         System.out.println("\nDynarray size = " + da.size() + "\n");
 //        da.remove(2);
         da.remove(3);
-        Arrays.stream(da.array).forEach(System.out::println);
-
+        for (int i = 0; i < da.size(); i++) System.out.println(da.get(i));
     }
 }
