2c0e0b2d82bd084a9d02b77678940b867199f50c
smartglobus <smartglobus@gmail.com>
2023-06-13 20:45:17 +0300

    smartglobus study

--- src/AM_archiver/Exercises.java
+++ src/AM_archiver/Exercises.java
@@ -7,127 +7,121 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exercises {
-    private final static int WINDOW = 300;
-    private final static int MIN_CODE = 3;
-    private int maxBuf = 30;
+    private final static int WINDOW = 16384; // размер скользящего окна
+    private final static int MIN_CODE = 3; // минимальная длина кодируемой повторяющейся последовательности
+    private final int BUF_LIM = 15 + MIN_CODE; // максимальный размер буфера сравнения, т.е. максимальная длина кодируемой повторяющейся последовательности
     private int windowStartPosition;
+    private int length;
+    private int distance;
 
     private List<Link> tempLinks = new ArrayList<>();
     private List<List<Byte>> vocabulary; // or Map ???
     private int wordPosition;
 
+    // отладочные переменные
     private int inpFileSize; // отладка
     private int sumLength; // отладка
+    private int sumDistance; // отладка
+    private int minDistance = WINDOW; // отладка
 
     void readFile(String fileName, String outName) {
         try (FileInputStream fileInputStream = new FileInputStream(fileName);
              FileOutputStream fileOutputStream = new FileOutputStream(outName)) {
 
-                byte[] bytes = fileInputStream.readAllBytes();
-                inpFileSize = bytes.length; // отладка
-//            byte[] bytes = new byte[]{0, 1, 2, 3, 6, 8, 3, 6, 2, 3, 6, 8, 3, 4, 2, 7, 5, 3, 7, 2, 3, 6, 8, 6, 5, 7, 8, 9, 5, 5, 7, 8, 0};
+            byte[] bytes = fileInputStream.readAllBytes();
+            inpFileSize = bytes.length; // отладка
 //            for (byte b : bytes) System.out.print(b + "_");
-            // проход по массиву bytes
-            // i - позиция указателя начала буфера
-            for (int i = MIN_CODE; i < bytes.length - maxBuf; i++) { // последниЙ кусок размера maxBuf не кодируем
+            // bufStart - позиция указателя начала буфера, т.е. начала кодируемого участка
+            for (int bufStart = MIN_CODE; bufStart < bytes.length - BUF_LIM; bufStart++) { // последниЙ кусок размера BUF_LIM не кодируем
+// записываем первые биты (от 0 до MIN_CODE-1)без кодировки
+                windowStartPosition = (bufStart - WINDOW) >= 0 ? bufStart - WINDOW : 0; // вычислить начало окна windowStartPosition
+                Link link = new Link(MIN_CODE); // надо ли каждый шаг содавать новый объект? Нужен ли вообще объект???
 
-                windowStartPosition = (i - WINDOW) >= 0 ? i - WINDOW : 0; // вычислить начало окна windowStartPosition
-                Link link = new Link(0, MIN_CODE); // надо ли каждый шаг содавать новый объект? Нужен ли вообще объект???
                 // цикл поиска совпадения символа в окне с первым символом буфера
-                for (int wp = windowStartPosition; wp < i - MIN_CODE; wp++) {
+                for (int wndPos = windowStartPosition; wndPos < bufStart; wndPos++) {
 
-                    int lengthCount = 0;
-                    if (bytes[wp] == bytes[i]) {
-                        lengthCount++;
-                        for (int j = 1; j < maxBuf && j < i - wp; j++) { // j не больше размера буфера и не даёт [wp+j] "залезть" в буфер
-                            if (bytes[wp + j] == bytes[i + j]) {
+                    int lengthCount = 0; // длина текущей п-ти совпадений
+                    if (bytes[wndPos] == bytes[bufStart]) {
+                        for (int j = 0; j < BUF_LIM && j <= bufStart - wndPos; j++) {// j не больше  BUF_LIM, и (wndPos + j) не превышает bufStart
+
+                            if (bytes[wndPos + j] == bytes[bufStart + j]) { // подсчёт подряд совпадающих пар
                                 lengthCount++;
 
-                            } else {
-                                if (lengthCount >= link.length) {
-                                    link.distance = i - wp;
-                                    link.length = lengthCount;
-                                    wp += lengthCount - 1;
-                                }
-                                break;
+                                // блок проверки на многократно повторяющиеся последовательности
+                                if (wndPos + j == bufStart - 1) { // если совпадения продолжились до конца окна
+                                    j++;
+                                    for (int i = 0; i <= j && j < BUF_LIM; i++, j++) {
+                                        if (i == j) i = 0; // i крутится от 0 до j
+                                        if (bytes[wndPos + i] == bytes[bufStart + j]) {// если следующий байт буфера равен первому символу кодируемой п-ти
+                                            lengthCount++;
+                                        }else break;
+                                    }
                             }
+
+
+                            // блок создания ссылки
+                            if (lengthCount >= link.length) {
+                                link.linkPosition = bufStart; // отладка
+                                link.distance = bufStart - wndPos; // wndPos здесь - первый бит повторяющейся последовательности
+                                link.length = lengthCount;
+                            }
+                        } else{
+
+                            break;
                         }
-
-//
-//                        link.distance = i - wp; // ??? - check!!!
-//                        link.length = 3;
-//
-//                        currDistance = i - wp;
-//                        currLength = 3;
-
-// здесь идёт цикл по наращиванию буфера c сохранением последовательности в промежуточный словарь
-// при наращивании буфера следить, чтобы не выйти за конец массива bytes (сделал, ограничив максимум i в основном цикле)
-//                        repSeq.add(bytes[wp++]);
-//                        repSeq.add(bytes[wp++]);
-//                        repSeq.add(bytes[wp++]);
-
-//                        for (int n = i + 3; n < i || repSeq.size()< maxBuf; n++) {
-//                            if (bytes[wp] == bytes[n]) {
-//                                repSeq.add(bytes[wp++]);
-//                            }
-//                        }
-
-
-                    } else {
-                        // записываем символы без кодирования
                     }
-                }
-                if (link.distance > 0) {
-                    tempLinks.add(link); // записать Link в результат
-                    i += link.length;
-                    sumLength += link.length; // отладка
+
+                } else{
+                    // записываем бит без кодирования
                 }
             }
+            if (link.distance > 0) {
+                tempLinks.add(link); // записать Link в результат
+                bufStart += link.length;
 
-            tempLinks.stream().filter(e -> e.length > 0).forEach(e -> System.out.println(e.distance + " " + e.length));
-            fileOutputStream.write(bytes);
-        } catch (IOException e) {
-            e.printStackTrace();
+                sumLength += link.length; // отладка
+                sumDistance += link.distance; // отладка
+                if (link.distance < minDistance) minDistance = link.distance; // отладка
+            }
         }
+
+        tempLinks.stream().filter(e -> e.length > 0).forEach(e -> System.out.println("pos " + e.linkPosition + ", dist = " + e.distance + ", lenght " + e.length));
+        fileOutputStream.write(bytes);
+    } catch(
+    IOException e)
+
+    {
+        e.printStackTrace();
+    }
+}
+
+class Link {
+    int linkPosition; // отладка
+    int distance;
+    int length;
+
+    public Link(int length) {
+        this.length = length;
     }
 
-    class Link {
-        int distance;
-        int length;
+}
 
-        public Link(int distance, int length) {
-            this.distance = distance;
-            this.length = length;
-        }
+    // возвращает 16 бит, из которых первые 12 кодируют distance, а остальные 4 length
+    private short linkShort(int distance, int length) {
+// длина последовательности от 3 до 18 (length-3 помещается в 4 бита, от 0 до 15)
+        return 0;
     }
 
-
-    void compress() {
-
-    }
 
     public static void main(String[] args) {
         Exercises exr = new Exercises();
-        exr.readFile("C:\\Users\\User\\Pictures\\BachMonogrm1.jpg", "C:\\Users\\User\\Pictures\\BachMonogrm1_copy.jpg");
-        String song = "Слышу голос из Прекрасного Далека,\n" +
-                "Голос утренний в серебряной росе.\n" +
-                "Слышу голос, и манящая дорога\n" +
-                "Кружит голову, как в детстве карусель.\n" +
-                "\n" +
-                "Припев:\n" +
-                "Прекрасное Далеко,\n" +
-                "Не будь ко мне жестоко,\n" +
-                "Не будь ко мне жестоко -\n" +
-                "Жестоко не будь.\n" +
-                "От чистого истока\n" +
-                "В Прекрасное Далеко,\n" +
-                "В Прекрасное Далеко\n" +
-                "Я начинаю путь.";
-        char[] songToComp = song.toCharArray();
-        byte[] bytes = song.getBytes();
-        for (char ch : songToComp) {
-//            System.out.println(ch);
-        }
-        System.out.println(((exr.inpFileSize - exr.sumLength)*100/exr.inpFileSize) + "%");
+        exr.readFile("C:\\Users\\User\\IdeaProjects\\Progwards first project\\src\\AM_archiver\\file1.txt", "C:\\Users\\User\\Pictures\\BachMonogrm1_copy.jpg");
+
+
+        System.out.println("\nсжатие до " + ((exr.inpFileSize - exr.sumLength) * 100 / exr.inpFileSize) + "%");
+        System.out.println(WINDOW);
+        System.out.println("средняя дистанция " + exr.sumDistance / exr.tempLinks.size());
+        System.out.println("средняя длина " + exr.sumLength / exr.tempLinks.size());
+        System.out.println("minDistance " + exr.minDistance);
     }
 }
