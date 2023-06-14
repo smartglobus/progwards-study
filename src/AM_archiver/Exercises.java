80741702865819b9dd3059166ea53c55bc4ba1bc
smartglobus <smartglobus@gmail.com>
2023-06-14 21:51:54 +0300

    smartglobus study

--- src/AM_archiver/Exercises.java
+++ src/AM_archiver/Exercises.java
@@ -11,12 +11,13 @@ public class Exercises {
     private final static int MIN_CODE = 3; // минимальная длина кодируемой повторяющейся последовательности
     private final int BUF_LIM = 15 + MIN_CODE; // максимальный размер буфера сравнения, т.е. максимальная длина кодируемой повторяющейся последовательности
     private int windowStartPosition;
+    private ArrayList<Byte> outBytes;
+    private ArrayList<Byte> codedBlock;// блок из служебного байта-указателя и 8 некодированных байтов или ссылок
     private int length;
     private int distance;
 
     private List<Link> tempLinks = new ArrayList<>();
-    private List<List<Byte>> vocabulary; // or Map ???
-    private int wordPosition;
+
 
     // отладочные переменные
     private int inpFileSize; // отладка
@@ -54,62 +55,60 @@ public class Exercises {
                                         if (i == j) i = 0; // i крутится от 0 до j
                                         if (bytes[wndPos + i] == bytes[bufStart + j]) {// если следующий байт буфера равен первому символу кодируемой п-ти
                                             lengthCount++;
-                                        }else break;
+                                        } else break;
                                     }
+                                }
+
+
+                                // блок создания ссылки
+                                if (lengthCount >= link.length) {
+                                    link.linkPosition = bufStart; // отладка
+                                    link.distance = bufStart - wndPos; // wndPos здесь - первый бит повторяющейся последовательности
+                                    link.length = lengthCount;
+                                }
+                            } else {
+
+                                break;
                             }
-
-
-                            // блок создания ссылки
-                            if (lengthCount >= link.length) {
-                                link.linkPosition = bufStart; // отладка
-                                link.distance = bufStart - wndPos; // wndPos здесь - первый бит повторяющейся последовательности
-                                link.length = lengthCount;
-                            }
-                        } else{
-
-                            break;
                         }
-                    }
 
-                } else{
-                    // записываем бит без кодирования
+                    } else {
+                        // записываем бит без кодирования
+                    }
+                }
+                if (link.distance > 0) {
+                    tempLinks.add(link); // записать Link в результат
+                    bufStart += link.length;
+
+                    sumLength += link.length; // отладка
+                    sumDistance += link.distance; // отладка
+                    if (link.distance < minDistance) minDistance = link.distance; // отладка
                 }
             }
-            if (link.distance > 0) {
-                tempLinks.add(link); // записать Link в результат
-                bufStart += link.length;
 
-                sumLength += link.length; // отладка
-                sumDistance += link.distance; // отладка
-                if (link.distance < minDistance) minDistance = link.distance; // отладка
-            }
+            tempLinks.stream().filter(e -> e.length > 0).forEach(e -> System.out.println("pos " + e.linkPosition + ", dist = " + e.distance + ", lenght " + e.length));
+
+//            fileOutputStream.write(outBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class Link {
+        int linkPosition; // отладка
+        int distance;
+        int length;
+
+        public Link(int length) {
+            this.length = length;
         }
 
-        tempLinks.stream().filter(e -> e.length > 0).forEach(e -> System.out.println("pos " + e.linkPosition + ", dist = " + e.distance + ", lenght " + e.length));
-        fileOutputStream.write(bytes);
-    } catch(
-    IOException e)
-
-    {
-        e.printStackTrace();
     }
-}
-
-class Link {
-    int linkPosition; // отладка
-    int distance;
-    int length;
-
-    public Link(int length) {
-        this.length = length;
-    }
-
-}
 
     // возвращает 16 бит, из которых первые 12 кодируют distance, а остальные 4 length
     private short linkShort(int distance, int length) {
 // длина последовательности от 3 до 18 (length-3 помещается в 4 бита, от 0 до 15)
-        return 0;
+        return (short) ((distance<<3) + length);
     }
 
 
