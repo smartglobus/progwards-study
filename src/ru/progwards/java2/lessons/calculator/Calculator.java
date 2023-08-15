158617e2e8dcc67442659b6973d12090ed9b905f
smartglobus <smartglobus@gmail.com>
2023-08-15 13:29:56 +0300

    smartglobus study

--- src/ru/progwards/java2/lessons/calculator/Calculator.java
+++ src/ru/progwards/java2/lessons/calculator/Calculator.java
@@ -18,57 +18,58 @@ public class Calculator {
         return expr.substring(pos, ++pos);
     }
 
-    String chkNxtSym() {
+    String chkSym() {
         return expr.substring(pos, pos + 1);
     }
-
 
     public int sum() {
         int res = mult();
 
         while (hasNext()) {
-            String nxtOp = getSym();
+            String nxtOp = chkSym();
             if ("+-".contains(nxtOp)) {
-                switch (nxtOp) {
-                    case "+":
-                        res += mult();
-                        break;
-                    case "-":
-                        res -= mult();
-                        break;
-                }
-            }
-        }
-        return res;
-    }
-
-    int mult() {
-        // возвращает или результат */ , или просто цифру
-        int res = Integer.parseInt(getSym());
-        while (hasNext()) {
-            String nxtOp = chkNxtSym();
-            if ("*/".contains(nxtOp)) {
                 pos++;
-                int nxtNum = Integer.parseInt(getSym());
-                switch (nxtOp){
-                    case "*":
-                        res *=  nxtNum;
-                        break;
-                    case "/":
-                        res /=  nxtNum;
-                        break;
-                }
+                int nxtNum =mult();
+                if ("+".equals(nxtOp)) res += nxtNum;
+                if ("-".equals(nxtOp)) res -= nxtNum;
             } else return res;
         }
         return res;
     }
 
+    // Для варианта без обработки скобок вместо метода par() вызывается Integer.parseInt(getSym())
+    int mult() {
+        int res = par();// = Integer.parseInt(getSym());
+        while (hasNext()) {
+            String nxtOp = chkSym();
+            if ("*/".contains(nxtOp)) {
+                pos++;
+                int nxtNum = par();// = Integer.parseInt(getSym());
+                if ("*".equals(nxtOp)) res *= nxtNum;
+                if ("/".equals(nxtOp)) res /= nxtNum;
+            } else return res;
+        }
+        return res;
+    }
+
+    int par() {
+        int res;
+        if ("(".equals(chkSym())) {
+            pos++;
+            res = sum();
+            if (")".equals(chkSym())) {
+                pos++;
+                return res;
+            }
+        }
+        return Integer.parseInt(getSym());
+    }
 
     public static int calculate(String expression) {
         return new Calculator(expression).sum();
     }
 
     public static void main(String[] args) {
-        System.out.println(calculate("2+8*6/4+3+5-3*2"));
+        System.out.println(calculate("2+8*((6-4)*2-1)-3/2"));
     }
 }
