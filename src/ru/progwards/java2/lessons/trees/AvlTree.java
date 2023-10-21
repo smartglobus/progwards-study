11e418b94aacfad1a9843e53f95ce20c55b4ee44
smartglobus <smartglobus@gmail.com>
2023-10-22 02:19:22 +0300

    working version, finally!

--- src/ru/progwards/java2/lessons/trees/AvlTree.java
+++ src/ru/progwards/java2/lessons/trees/AvlTree.java
@@ -39,7 +39,6 @@ public class AvlTree<K extends Comparable<K>, V> {
         }
 
         private void add(TreeLeaf<K, V> leaf) {
-//здесь происходит добавление узла в дерево и, при необходимости, балансировка нужными вращениями
             int cmp = leaf.key.compareTo(key);
             if (cmp == 0) this.value = leaf.value;
             if (cmp > 0) {
@@ -79,15 +78,22 @@ public class AvlTree<K extends Comparable<K>, V> {
     public void delete(K key) {
         TreeLeaf<K, V> leafToDel = root.find(key);
         if (leafToDel.key.compareTo(key) != 0) return;// ключ не найден
-        if (leafToDel.height > 0) { // (leafToDel.left != null || leafToDel.right != null) ???
-            TreeLeaf<K, V> replace = leafToDel.getBalance() > 0 ? findMin(leafToDel.right) : findMax(leafToDel.left);
+        if (leafToDel.height > 0) { // (leafToDel.left != null || leafToDel.right != null)
+            boolean bal = leafToDel.getBalance() > 0;
+            TreeLeaf<K, V> replace = bal ? findMin(leafToDel.right) : findMax(leafToDel.left);
             TreeLeaf<K, V> replaceParent = replace.parent;
             K replaceParentKey = replace.parent.key;
             leafToDel.key = replace.key;
             leafToDel.value = replace.value;
-            if (replaceParentKey.compareTo(replace.key) > 0)
-                replaceParent.left = null;// удаление ссылки на узел replace
-            else replaceParent.right = null;
+            if (bal) {
+                if (replaceParentKey.compareTo(replace.key) > 0) replaceParent.left = replace.right;
+                else replaceParent.right = replace.right;
+                if (replace.right != null) replace.right.parent = replaceParent;
+            } else {
+                if (replaceParentKey.compareTo(replace.key) > 0) replaceParent.left = replace.left;
+                else replaceParent.right = replace.left;
+                if (replace.left != null) replace.left.parent = replaceParent;
+            }
             fixHeightAndBalance(replaceParent);
         } else { // узел не имеет потомков
             if (leafToDel == root) {
@@ -101,17 +107,16 @@ public class AvlTree<K extends Comparable<K>, V> {
         }
     }
 
-    private TreeLeaf<K, V> balance(TreeLeaf<K, V> leaf) {
-// Проверка наличия и вида дисбаланса. Возврат - новая (локальная) вершина дерева
+    private void balance(TreeLeaf<K, V> leaf) {
+// Проверка наличия и вида дисбаланса.
         if (leaf.getBalance() == -2) {
-            if (leaf.left.getBalance() <= 0) return rightRotation(leaf);
-            else return rightRotation(leftRotation(leaf.left).parent);// большое правое вращение (МЛВ + МПВ)
+            if (leaf.left.getBalance() <= 0) rightRotation(leaf);
+            else rightRotation(leftRotation(leaf.left).parent);// большое правое вращение (МЛВ + МПВ)
         }
         if (leaf.getBalance() == 2) {
-            if (leaf.right.getBalance() >= 0) return leftRotation(leaf);
-            else return leftRotation(rightRotation(leaf.right).parent);// большое правое вращение (МПВ + МЛВ)
+            if (leaf.right.getBalance() >= 0) leftRotation(leaf);
+            else leftRotation(rightRotation(leaf.right).parent);// большое правое вращение (МПВ + МЛВ)
         }
-        return leaf;
     }
 
     // Малое левое вращение (МЛВ). Bозвращает новую (локальную) вершину дерева
@@ -196,28 +201,38 @@ public class AvlTree<K extends Comparable<K>, V> {
         for (int i = 0; i < 10; i++) {
 //            int rdm = Math.abs(ThreadLocalRandom.current().nextInt() % 50);
 //            myPunyTree.put(rdm, rdm);
-            myPunyTree.put(i, i);
+//            myPunyTree.put(i, i);
         }
-//        myPunyTree.put(7, 7);
+        int[] array = {5, 11, 3, 8, 6, 4, 0, 2, 7, 1, 9, 67, 34};
+        for (int i = 0; i < array.length; i++) {
+            myPunyTree.put(array[i], array[i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            myPunyTree.delete(array[i]);
+
+        }
 //        myPunyTree.put(12, 12);
 //        myPunyTree.put(11, 11);
 //        myPunyTree.put(10, 10);
 //        myPunyTree.put(6, 6);
 //
 //        myPunyTree.put(5, 5);
-        myPunyTree.root.process(v -> System.out.println(v.key));
+
 //        myPunyTree.put(9, 9);
 //        myPunyTree.put(8, 8);
-        try {
-            System.out.println(myPunyTree.find(3));
-            System.out.println(myPunyTree.find(69));
-            System.out.println(myPunyTree.find(6));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        myPunyTree.delete(8);
-        myPunyTree.delete(9);
+//        myPunyTree.root.process(v -> System.out.println(v.key));
+//        try {
+//            System.out.println(myPunyTree.find(3));
+//            System.out.println(myPunyTree.find(69));
+//            System.out.println(myPunyTree.find(6));
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+        System.out.println();
+//        myPunyTree.delete(7);
+//        myPunyTree.delete(8);
+//        myPunyTree.delete(9);
 
         System.out.println();
     }
