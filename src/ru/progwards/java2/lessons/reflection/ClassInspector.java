7adbcd2a1d697a120c106ce2ea88de11cf8d1328
smartglobus <smartglobus@gmail.com>
2023-10-28 21:52:51 +0300

    ready version

--- src/ru/progwards/java2/lessons/reflection/ClassInspector.java
+++ src/ru/progwards/java2/lessons/reflection/ClassInspector.java
@@ -1,5 +1,7 @@
 package ru.progwards.java2.lessons.reflection;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -7,49 +9,52 @@ import java.lang.reflect.Modifier;
 
 public class ClassInspector {
     public static void inspect(String clazz) throws ClassNotFoundException {
-
         Class cls = Class.forName(clazz);
-        System.out.print(Modifier.toString(cls.getModifiers()) + " class ");
-
         String[] clsName = cls.getName().split("\\.");
+        String clsSimpleName = clsName[clsName.length - 1];
+        try (FileWriter writer = new FileWriter(clsSimpleName + ".java")) {
+            writer.write(Modifier.toString(cls.getModifiers()) + " class ");
+            writer.write(clsSimpleName + " {");
+            Field[] fields = cls.getDeclaredFields();
+            Constructor[] constructors = cls.getDeclaredConstructors();
+            Method[] methods = cls.getDeclaredMethods();
 
-        System.out.print(clsName[clsName.length - 1] + "{\n");
-        Field[] fields = cls.getDeclaredFields();
-        Constructor[] constructors = cls.getDeclaredConstructors();
-        Method[] methods = cls.getDeclaredMethods();
-
-        for (Field f : fields) {
-            System.out.print("\t" + Modifier.toString(f.getModifiers()) + " ");
-            System.out.println(f.getName() + ";");
-        }
-        for (Constructor c : constructors) {
-            System.out.print("\t" + Modifier.toString(c.getModifiers()) + " ");
-            String[] name = c.getName().split("\\.");
-            System.out.print(name[name.length - 1] + "(");
-            Class[] classes = c.getParameterTypes();
-            if (classes.length == 0) System.out.println(") {}");
-            for (int i = 0; i < classes.length; i++) {
-                Class parCls = classes[i];
-                System.out.print(parCls.getSimpleName());
-                System.out.print(i == classes.length - 1 ? " arg" + i + ") {}\n" : " arg" + i + ", ");
+            for (Field f : fields) {
+                writer.write("\n\t" + Modifier.toString(f.getModifiers()) + " ");
+                String[] fType = f.getType().toString().split("\\.");
+                writer.write(fType[fType.length - 1] + " ");
+                writer.write(f.getName() + ";");
             }
-        }
-        for (Method m : methods) {
-            System.out.print("\t" + Modifier.toString(m.getModifiers()) + " ");
-            System.out.print(m.getName() + "(");
-            Class[] classes = m.getParameterTypes();
-            if (classes.length == 0) System.out.println(") {}");
-            for (int i = 0; i < classes.length; i++) {
-                Class parCls = classes[i];
-                System.out.print(parCls.getSimpleName());
-                System.out.print(i == classes.length - 1 ? " arg" + i + ") {}\n" : " arg" + i + ", ");
+            for (Constructor c : constructors) {
+                writer.write("\n\t" + Modifier.toString(c.getModifiers()) + " ");
+                String[] name = c.getName().split("\\.");
+                writer.write(name[name.length - 1] + "(");
+                Class[] classes = c.getParameterTypes();
+                if (classes.length == 0) writer.write(") {}");
+                for (int i = 0; i < classes.length; i++) {
+                    Class parCls = classes[i];
+                    writer.write(parCls.getSimpleName());
+                    writer.write(i == classes.length - 1 ? " arg" + i + ") {}" : " arg" + i + ", ");
+                }
             }
+            for (Method m : methods) {
+                writer.write("\n\t" + Modifier.toString(m.getModifiers()) + " ");
+                writer.write(m.getName() + "(");
+                Class[] classes = m.getParameterTypes();
+                if (classes.length == 0) writer.write(") {}");
+                for (int i = 0; i < classes.length; i++) {
+                    Class parCls = classes[i];
+                    writer.write(parCls.getSimpleName());
+                    writer.write(i == classes.length - 1 ? " arg" + i + ") {}" : " arg" + i + ", ");
+                }
+            }
+            writer.write("\n}");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("}");
     }
 
     public static void main(String[] args) throws ClassNotFoundException {
-
         inspect("ru.progwards.java2.lessons.reflection.Employee");
     }
 }
