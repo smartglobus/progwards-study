099f8d86b621df9717e9ca567b363ee47e0971e2
smartglobus <smartglobus@gmail.com>
2023-11-07 14:54:47 +0300

    smartglobus study

--- src/ru/progwards/java2/lessons/reflection/GettersAndSetters.java
+++ src/ru/progwards/java2/lessons/reflection/GettersAndSetters.java
@@ -1,7 +1,9 @@
 package ru.progwards.java2.lessons.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,25 +13,49 @@ public class GettersAndSetters {
         Class cls = Class.forName(clazz);
         Field[] fields = cls.getDeclaredFields();
         List<String> methodsNames = new ArrayList<>();
-        Arrays.stream(cls.getDeclaredMethods()).forEach(method -> methodsNames.add(method.getName()));
+        Method[] methods = cls.getDeclaredMethods();
+        Arrays.stream(methods).forEach(method -> methodsNames.add(method.getName()));
 
         for (Field f : fields) {
             String modifiers = Modifier.toString(f.getModifiers());
             if (modifiers.contains("private")) {
                 String fName = f.getName();
-                String fType = f.getType().getSimpleName();
+                String fTypeName = f.getType().getSimpleName();
                 String fNameFirstCapitalLetter = fName.substring(0, 1).toUpperCase() + fName.substring(1);
-                if (!methodsNames.contains("set" + fNameFirstCapitalLetter))
-                    System.out.println("public void set" + fNameFirstCapitalLetter + "(" + fType + " " + fName + ")");
-                if (!methodsNames.contains("get" + fNameFirstCapitalLetter))
-                    System.out.println("public " + fType + " get" + fNameFirstCapitalLetter + "()");
+                String setter = "set" + fNameFirstCapitalLetter;
+                String getter = "get" + fNameFirstCapitalLetter;
+                if (!methodsNames.contains(setter))
+                    System.out.println("public void " + setter + "(" + fTypeName + " " + fName + ")");
+                else {
+                    try {
+                        Method method = cls.getDeclaredMethod(setter, f.getType());
+                        String methodModifiers = Modifier.toString(method.getModifiers());
+                        if (!methodModifiers.contains("public") || methodModifiers.contains("static"))
+                            System.out.println("public void " + setter + "(" + fTypeName + " " + fName + ")");
+                    } catch (NoSuchMethodException e) {
+                        System.out.println("public void " + setter + "(" + fTypeName + " " + fName + ")");
+                    }
+                }
+                if (!methodsNames.contains(getter))
+                    System.out.println("public " + fTypeName + " " + getter + "()");
+                else {
+                    try {
+                        Method method = cls.getDeclaredMethod(getter);
+                        String methodModifiers = Modifier.toString(method.getModifiers());
+                        if (!methodModifiers.contains("public") || methodModifiers.contains("static"))
+                            System.out.println("public " + fTypeName + " " + getter + "()");
+                    } catch (NoSuchMethodException e) {
+                        System.out.println("public " + fTypeName + " " + getter + "()");
+                    }
+                }
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         try {
             check("ru.progwards.java2.lessons.reflection.Employee");
+            System.out.println("--------------");
             check("ru.progwards.java2.lessons.gc.Heap");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
