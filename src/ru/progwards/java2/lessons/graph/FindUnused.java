fc9ea8b95a791f40ebc1f7812c579a83141dccea
smartglobus <smartglobus@gmail.com>
2024-01-02 22:36:00 +0300

    smartglobus study

---
+++ src/ru/progwards/java2/lessons/graph/FindUnused.java
@@ -0,0 +1,43 @@
+package ru.progwards.java2.lessons.graph;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindUnused {
+    List<CObject> roots; // -  список корневых объектов
+    List<CObject> objects; // - список всех объектов системы
+
+    public static List<CObject> find(List<CObject> roots, List<CObject> objects) {
+
+        // предварительная пометка как неиспользуемые для всех объектов-потомков неиспользуемых корней
+        for (CObject c : roots) if (c.mark == 0) markUnused(c);
+
+        for (CObject c : roots) if (c.mark != 0) markUsed(c);
+
+        return objects.stream().filter(cObject -> cObject.mark != 0).collect(Collectors.toList());
+    }
+
+    private static void markUnused(CObject cObject) {
+        cObject.mark = 0;
+        for (CObject c : cObject.references) markUnused(c);
+    }
+
+    private static void markUsed(CObject cObject) {
+// Если метка 1, значит объект уже в обработке. Сам в своё время получит метку 2 (если он не в циклической части графа, что допустимо).
+// Если метка 2, обработка уже не нужна
+        if (cObject.mark == 0) {
+            cObject.mark = 1; // метка ставится на случай, если объект окажется одним из корней, ранее считавшимся неиспользуемым
+            for (CObject c : cObject.references)
+                markUsed(cObject);
+            cObject.mark = 2;
+        }
+    }
+
+    class CObject {
+        public List<CObject> references; // ссылки на другие объекты
+        int mark;  // пометка для алгоритма
+        // 0 - не используется
+        // 1 - посещен
+        // 2 - используется
+    }
+}
