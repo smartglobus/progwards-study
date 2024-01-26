cd16e4a97d2a86c55b630a4ce0041a009a79631c
smartglobus <smartglobus@gmail.com>
2024-01-26 20:40:31 +0300

    smartglobus study

---
+++ src/ru/progwards/java2/lessons/threads/Summator.java
@@ -0,0 +1,49 @@
+package ru.progwards.java2.lessons.threads;
+
+import java.math.BigInteger;
+
+public class Summator {
+    private int count;
+    BigInteger result = BigInteger.ZERO;
+
+    public Summator(int count) {
+        this.count = count;
+    }
+
+    public BigInteger sum(BigInteger number) throws InterruptedException {
+        Thread[] threads = new Thread[count];
+        BigInteger step = number.divide(BigInteger.valueOf(count));
+
+        for (int i = 0; i < count; i++) {
+            int j = i;
+            threads[i] = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    BigInteger from = BigInteger.valueOf(j).multiply(step);
+                    BigInteger to;
+                    if (j == count - 1) to = number;
+                    else to = from.add(step);
+                    partSum(from.add(BigInteger.ONE), to);
+                }
+            });
+            threads[i].start();
+        }
+        for (int i = 0; i < count; i++) threads[i].join();
+        return result;
+    }
+
+    synchronized void partSum(BigInteger from, BigInteger to) {
+
+        int n = to.subtract(from).intValue() + 1;
+        for (int i = 0; i < n; i++) {
+            BigInteger x = from.add(BigInteger.valueOf(i));
+            result = result.add(x);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Summator summator = new Summator(7);
+        System.out.println(summator.sum(BigInteger.valueOf(11)));
+    }
+}
